d71164604b5a152359cb39a9e02a124f6ec546b1
sondrev <sonves@gmail.com>
2016-11-04 21:27:43 +0100

    s

--- main.spawn.js
+++ main.spawn.js
@@ -34,7 +34,7 @@ var mainSpawn = {
       for (var ii=0;ii<source.miners.length;ii++) {
         if (Game.getObjectById(source.miners[ii]) == null) {
         console.log('dedicated miner died. Remove');
-        source.miners.splice(index, ii);
+        source.miners.splice(ii, 1);
       }
     }
       //console.log('source ',i,'. Miners: ',source.miners.length,' / ',source.slots);
