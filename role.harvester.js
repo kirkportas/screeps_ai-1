bb27a066107cf664a3668f373d11a0d63e503843
sondrev <sonves@gmail.com>
2016-11-04 23:40:51 +0100

    s

--- role.harvester.js
+++ role.harvester.js
@@ -15,22 +15,13 @@ var roleHarvester = {
 
 	    if(creep.carry.energy < creep.carryCapacity) {
         if (creep.memory.pref) {
-          var source = Game.getObjectById(creep.memory.pref)
-          if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
-              creep.moveTo(source);
-          }
+          tasks.harvestPrefered(creep);
         } else {
-          var sources = creep.room.find(FIND_SOURCES);
-          if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-              creep.moveTo(sources[0]);
-          }
+          tasks.harvestClosest(creep);
         }
-
-        }
-        else {
-          tasks.deliverSource(creep);
-
-        }
+      } else {
+        tasks.deliverSource(creep);
+      }
 	}
 };
 
--- tasks.js
+++ tasks.js
@@ -17,6 +17,18 @@ var tasks = {
               creep.moveTo(targets[0]);
           }
       }
+    },
+    harvestClosest: function(creep) {
+      var sources = creep.room.find(FIND_SOURCES);
+      if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(sources[0]);
+      }
+    },
+    harvestPrefered: function(creep) {
+      var source = Game.getObjectById(creep.memory.pref)
+      if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(source);
+      }
     }
 
 
