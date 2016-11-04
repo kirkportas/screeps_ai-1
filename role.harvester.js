82bd0315a7675e4e7ba296596d6a3c56a6481299
sondrev <sonves@gmail.com>
2016-11-04 02:09:48 +0100

    sss

--- role.harvester.js
+++ role.harvester.js
@@ -4,9 +4,9 @@ var roleHarvester = {
     run: function(creep) {
 	    if(creep.carry.energy < creep.carryCapacity) {
         if (creep.memory.pref) {
-          var source = Game.getObjectById(creep.memory.pref)
-          if(creep.harvest(source == ERR_NOT_IN_RANGE) {
-              creep.moveTo(source);
+          //var source = Game.getObjectById(creep.memory.pref)
+          //if(creep.harvest(source == ERR_NOT_IN_RANGE) {
+          //    creep.moveTo(source);
           }
         } else {
           var sources = creep.room.find(FIND_SOURCES);
