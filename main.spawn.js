4131342753fc29d2734e94c77dc0b07d6904010b
sondrev <sonves@gmail.com>
2016-11-04 21:20:58 +0100

    s

--- main.spawn.js
+++ main.spawn.js
@@ -31,12 +31,7 @@ var mainSpawn = {
     var sources = Game.spawns['Spawn1'].room.memory.allSources;
     for (var i=0;i<sources.length;i++) {
       var source=sources[i];
-      //for (var ii=0;ii<source.miners.length;ii++) {
-        //if (Game.getObjectById(source.miners[ii]) == null) {
-          //console.log('dedicated miner died. Remove');
-          //source.miners.delete(source.miners[ii]);
-        //}
-      //}
+      //console.log('source ',i,'. Miners: ',source.miners.length,' / ',source.slots);
       if (source.miners.length<source.slots && source.safe) {
         if (Game.spawns['Spawn1'].canCreateCreep([WORK,CARRY,MOVE] == OK)) {
           var preferedSource = source.id;
