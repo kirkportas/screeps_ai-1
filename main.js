3cb015cc748ac97e32dd3bd319b8bd7e6413270c
sondrev <sonves@gmail.com>
2016-11-04 01:32:15 +0100

    s

--- main.js
+++ main.js
@@ -39,8 +39,8 @@ module.exports.loop = function () {
         var dist = 100;
         for (var ii = 0; ii < lairs.length; ii++) {
           Memory.test=lairs[i];
-          var a = x - lairs[i].x;
-          var b = y - lairs[i].y;
+          var a = x - lairs[i].pos.x;
+          var b = y - lairs[i].pos.y;
           var c = Math.sqrt( a*a + b*b );
             if (c<dist) dist=c;
           }
