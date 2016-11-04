dde9c039f33fcaeeec3c5305b5e94d862b324cfc
sondrev <sonves@gmail.com>
2016-11-05 00:22:52 +0100

    s

--- tasks.js
+++ tasks.js
@@ -37,8 +37,8 @@ var tasks = {
                   }
           });
           if(target!=null) {
-              if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-                  creep.moveTo(targets[0]);
+              if(creep.build(target) == ERR_NOT_IN_RANGE) {
+                  creep.moveTo(target);
               }
           }
 
