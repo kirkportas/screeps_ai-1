82566fb83e52ba963382cd52468757ffc7e2e46b
sondrev <sonves@gmail.com>
2016-11-04 21:00:04 +0100

    s

--- role.harvester.js
+++ role.harvester.js
@@ -5,7 +5,7 @@ var roleHarvester = {
         var sources = Game.spawns['Spawn1'].room.memory.allSources;
         for (var i=0;i<sources.length;i++) {
           var source=sources[i];
-          if (source.id === creep.memory.pref && !source.miners.includes(creep.id) && creep.id!=='undefined') {
+          if (source.id === creep.memory.pref && !source.miners.includes(creep.id) && creep.id != null) {
             console.log('adding ',creep.id);
             source.miners.push(creep.id);
           }
