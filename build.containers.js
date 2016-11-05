5218909338905712a935cd619a629246e6246395
sondrev <sonves@gmail.com>
2016-11-05 01:00:32 +0100

    s

--- build.containers.js
+++ build.containers.js
@@ -6,17 +6,20 @@ var buildContainers = {
         for (var s=0;s<sources.length;s++) {
           var source=sources[s];
           var sourceObj = Game.getObjectById(source.id);
-          var c=0;
+          var spots = [];
           for (var x1=-1;x1<2;x1++) { //LOOP though spots around source
             for (var y1=-1;y1<2;y1++) {
               var items = Game.spawns['Spawn1'].room.lookAt(sourceObj.pos.x+x1,sourceObj.pos.y+y1);
               for (let i=0;i<items.length;i++) {
-                  if (items[i].terrain=='plain' || items[i].terrain=='swamp') {c++;}
+                  if (items[i].terrain=='plain' || items[i].terrain=='swamp') {
+                    var newPos = new RoomPosition(sourceObj.pos.x+x1,sourceObj.pos.y+y1);
+                    if (!spots.includes(newPos)) {spots.push(newPos);}
+                  }
               }
 
             }
           }
-          console.log(c);
+          console.log(spots.length);
         }
 
 
