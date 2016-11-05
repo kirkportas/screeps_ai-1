6e01ba4e74fafb863ae822ead28e536b0c2d4f1b
sondrev <sonves@gmail.com>
2016-11-05 23:23:51 +0100

    s

--- main.spawn.js
+++ main.spawn.js
@@ -51,7 +51,7 @@ var mainSpawn = {
     }
 
   if (containers.length>=1) {
-    if(haulers < 1 && containers>=1) {
+    if(haulers < 1) {
         var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], findNextName('hauler'), {role: 'hauler'});
     } else  if(builders < 2) {
         var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], findNextName('builder'), {role: 'builder'});
