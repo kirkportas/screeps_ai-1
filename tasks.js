38aac8bd2e24360276bd9e3c7732497b5f90ed0b
sondrev <sonves@gmail.com>
2016-11-06 00:22:21 +0100

    s

--- tasks.js
+++ tasks.js
@@ -47,6 +47,14 @@ var tasks = {
     },
 
     haulFromDedicatedCotainers: function(creep) {
+      var spawn = creep.room.find(FIND_MY_STRUCTURES, {
+                      filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_SPAWN)
+                      }})[0];
+      var centralContainer=spawn.pos.findInRange(FIND_STRUCTURES,5, {
+                      filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_CONTAINER)
+                    }})[0];
       var targets = creep.room.find(FIND_STRUCTURES, {
                  filter: (structure) => {
                      return (
@@ -54,16 +62,13 @@ var tasks = {
                              ) && (structure.structureType == STRUCTURE_CONTAINER && structure.store[RESOURCE_ENERGY] > 50);
                  }
          });
-         if(targets.length > 0) {
-             if(creep.withdraw(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-               creep.moveTo(targets[0]);
-             }
-         } else {
-           var sources = creep.room.find(FIND_SOURCES);
-             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                 creep.moveTo(sources[0]);
-             }
+      var index = targets.indexOf(centralContainer);
+      if(index != -1) targets.splice( index, 1 );
+     if(targets.length > 0) {
+         if(creep.withdraw(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+           creep.moveTo(targets[0]);
          }
+     }
     },
 
   checkSourceNeeded: function(creep) {
