53b1ce372dfe6feed73cf903e7abacdf545e59e7
sondrev <sonves@gmail.com>
2016-11-05 23:48:52 +0100

    s

--- role.hauler.js
+++ role.hauler.js
@@ -1,3 +1,4 @@
+var tasks = require('tasks');
 var roleHauler = {
 
     /** @param {Creep} creep **/
@@ -13,39 +14,9 @@ var roleHauler = {
 	    }
 
 	    if(creep.memory.building) {
-            var targets = creep.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (
-                            structure.structureType == STRUCTURE_EXTENSION ||
-                            structure.structureType == STRUCTURE_SPAWN
-                            //structure.structureType == STRUCTURE_TOWER
-                                ) && (structure.energy < structure.energyCapacity);
-                    }
-            });
-            if(targets.length > 0) {
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0]);
-                }
-                }
-	    }
+        tasks.haulFromDedicatedCotainers(creep);
 	    else {
-	       var targets = creep.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (
-                            structure.structureType == STRUCTURE_CONTAINER
-                                ) && (structure.structureType == STRUCTURE_CONTAINER && structure.store[RESOURCE_ENERGY] > 50);
-                    }
-            });
-            if(targets.length > 0) {
-                if(creep.withdraw(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                	creep.moveTo(targets[0]);
-                }
-            } else {
-    	        var sources = creep.room.find(FIND_SOURCES);
-                if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(sources[0]);
-                }
-            }
+        tasks.deliverSourceToMain(creep);
 	    }
 	}
 };
--- tasks.js
+++ tasks.js
@@ -18,6 +18,43 @@ var tasks = {
           }
       }
     },
+    deliverSourceToMain: function(creep) {
+      var targets = creep.room.find(FIND_STRUCTURES, {
+              filter: (structure) => {
+                  return (
+                      structure.structureType == STRUCTURE_CONTAINER ||
+                      structure.structureType == STRUCTURE_EXTENSION ||
+                      structure.structureType == STRUCTURE_SPAWN
+                          ) && (structure.energy < structure.energyCapacity) || (structure.structureType == STRUCTURE_CONTAINER && structure.store[RESOURCE_ENERGY] < structure.storeCapacity);
+              }
+      });
+      _.sortBy(targets, s => creep.pos.getRangeTo(s))
+      if(targets.length > 0) {
+          if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+              creep.moveTo(targets[0]);
+          }
+      }
+    },
+
+    haulFromDedicatedCotainers: function(creep) {
+      var targets = creep.room.find(FIND_STRUCTURES, {
+                 filter: (structure) => {
+                     return (
+                         structure.structureType == STRUCTURE_CONTAINER
+                             ) && (structure.structureType == STRUCTURE_CONTAINER && structure.store[RESOURCE_ENERGY] > 50);
+                 }
+         });
+         if(targets.length > 0) {
+             if(creep.withdraw(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+               creep.moveTo(targets[0]);
+             }
+         } else {
+           var sources = creep.room.find(FIND_SOURCES);
+             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                 creep.moveTo(sources[0]);
+             }
+         }
+    }
 
   checkSourceNeeded: function(creep) {
     var targets = creep.room.find(FIND_MY_STRUCTURES, {
