091ed332389a02e2f99beb603956d0cea4a7976a
sondrev <sonves@gmail.com>
2016-11-06 22:50:03 +0100

    s

--- main.js
+++ main.js
@@ -8,25 +8,15 @@ var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
 
 module.exports.loop = function () {
-
+  for(var iRoom in Game.rooms) {
+    var room = Game.rooms[iRoom]
+    var spawn = room.find(FIND_MY_SPAWNS)[0];
+  }
 
     mainRoom.run();
     mainSpawn.run();
+    mainTower.run();
 
-    var tower = Game.getObjectById('fae2cfd64a4dd0ef19707798');
-    if(tower) {
-        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-            filter: (structure) => structure.hits < structure.hitsMax
-        });
-        if(closestDamagedStructure) {
-            tower.repair(closestDamagedStructure);
-        }
-
-        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-        if(closestHostile) {
-            tower.attack(closestHostile);
-        }
-    }
 
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
---
+++ main.tower.js
@@ -0,0 +1,19 @@
+var mainTower = {
+  run: function() {
+    var tower = Game.getObjectById('fae2cfd64a4dd0ef19707798');
+    if(tower) {
+        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+            filter: (structure) => structure.hits < structure.hitsMax
+        });
+        if(closestDamagedStructure) {
+            tower.repair(closestDamagedStructure);
+        }
+
+        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if(closestHostile) {
+            tower.attack(closestHostile);
+        }
+    }
+
+}
+module.exports = mainTower;
