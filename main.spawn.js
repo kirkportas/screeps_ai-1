1339a941c8c234c8f9bc76ad8487ee045b54e8ef
sondrev <sonves@gmail.com>
2016-11-06 19:42:10 +0100

    s

--- main.spawn.js
+++ main.spawn.js
@@ -30,9 +30,9 @@ var mainSpawn = {
 
     //cleanup dedicated miners
     var sources = Game.spawns['Spawn1'].room.memory.allSources;
-    Memory.test=sources;
     for (var i=0;i<sources.length;i++) {
       var source=sources[i];
+      Memory.test=sources[i];
       for (var ii=0;ii<source.miners.length;ii++) {
         if (Game.getObjectById(source.miners[ii]) == null) {
         console.log('dedicated miner died. Remove');
