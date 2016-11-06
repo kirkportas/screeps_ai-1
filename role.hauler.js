b346ebb20c2c7e364af0a72d20d1344250ccf0a1
sondrev <sonves@gmail.com>
2016-11-06 21:57:51 +0100

    s

--- role.hauler.js
+++ role.hauler.js
@@ -6,11 +6,9 @@ var roleHauler = {
 
 	    if(creep.memory.building && creep.carry.energy == 0) {
             creep.memory.building = false;
-            creep.say('getting');
 	    }
 	    if(!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
 	        creep.memory.building = true;
-	        creep.say('delivering');
 	    }
 
 	    if(creep.memory.building) {
