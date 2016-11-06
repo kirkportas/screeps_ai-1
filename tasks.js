74314bda0597d55ed745f8e5ef816af686ae8c0b
sondrev <sonves@gmail.com>
2016-11-06 01:15:30 +0100

    s

--- tasks.js
+++ tasks.js
@@ -75,6 +75,21 @@ var tasks = {
          }
      }
     },
+    haulFromContainerAny: function(creep) {
+      var targets = creep.room.find(FIND_STRUCTURES, {
+                 filter: (structure) => {
+                     return (
+                         structure.structureType == STRUCTURE_CONTAINER
+                             ) && (structure.structureType == STRUCTURE_CONTAINER && structure.store[RESOURCE_ENERGY] > 50);
+                 }
+         });
+         _.sortBy(targets, s => creep.pos.getRangeTo(s))
+         if(targets.length > 0) {
+             if(creep.withdraw(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+               creep.moveTo(targets[0]);
+             }
+
+    }
 
   checkSourceNeeded: function(creep) {
     var targets = creep.room.find(FIND_MY_STRUCTURES, {
