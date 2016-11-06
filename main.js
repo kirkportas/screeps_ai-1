72cc0f2851e4abf321ea57a7b38c3f5ebf6a47c1
sondrev <sonves@gmail.com>
2016-11-06 20:18:57 +0100

    s

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ module.exports.loop = function () {
 
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
-        //if(creep.memory.role == 'harvester') {roleHarvester.run(creep);}
+        if(creep.memory.role == 'harvester') {roleHarvester.run(creep);}
         if(creep.memory.role == 'harvesterBig') {roleHarvesterBig.run(creep);}
         if(creep.memory.role == 'hauler') {roleHauler.run(creep);}
         if(creep.memory.role == 'upgrader') {roleUpgrader.run(creep);}
