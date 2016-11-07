e08a6e9b24e10a4de0ea61f5e6aef575989d8f16
sondrev <sonves@gmail.com>
2016-11-07 17:55:43 +0100

    s

--- role.scout.js
+++ role.scout.js
@@ -30,9 +30,13 @@ var roleScout = {
           creep.moveTo(exit);
         } else {
           var containers = Game.rooms[firstRoom].find(FIND_MY_STRUCTURES);
+          var centralContainer=Game.spawns['Spawn1'].findInRange(FIND_STRUCTURES,5, {
+                          filter: (structure) => {
+                            return (structure.structureType == STRUCTURE_CONTAINER)
+                        }})[0];
           console.log('yaaaaay ',containers.length);
-          if (containers.length>0) {
-            creep.moveTo(containers[0]);
+          if (centralContainer) {
+            creep.moveTo(centralContainer);
           }
         }
 
