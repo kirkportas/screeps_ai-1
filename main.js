a8de68fa5c3c78c482f25e8cde64effc120f50cd
sondrev <sonves@gmail.com>
2016-11-07 17:12:11 +0100

    s

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ var mainRoom = require('main.room');
 var mainTower = require('main.tower');
 
 var roleHarvester = require('role.harvester');
-var roleHarvesterBig = require('role.harvesterBig');
 var roleHauler = require('role.hauler');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
@@ -36,7 +35,6 @@ module.exports.loop = function () {
         var creep = Game.creeps[name];
         //try {
           if(creep.memory.role == 'harvester') {roleHarvester.run(creep);}
-          if(creep.memory.role == 'harvesterBig') {roleHarvesterBig.run(creep);}
           if(creep.memory.role == 'hauler') {roleHauler.run(creep);}
           if(creep.memory.role == 'upgrader') {roleUpgrader.run(creep);}
           if(creep.memory.role == 'builder') {roleBuilder.run(creep);}
