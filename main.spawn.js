e90f949e5eee27d1a9be4c35c45a735ad1655d6f
sondrev <sonves@gmail.com>
2016-11-07 09:29:35 +0100

    s

--- main.spawn.js
+++ main.spawn.js
@@ -33,7 +33,7 @@ var mainSpawn = {
     });
 
 
-    var buildersNeeded = Math.max(1,Math.ceil(energyNeeded/2000));
+    var buildersNeeded = Math.max(1,Math.ceil( (energyNeeded/2000) + (repairNeeded/10000) ));
     console.log(constructionSites.length,' sites need energy: ', energyNeeded,' by builders: ',buildersNeeded,'. Damage to repair: ',repairNeeded);
 
 
