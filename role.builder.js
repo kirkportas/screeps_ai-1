3a1a241f5e782b3a1faf0e50c971aa7092d43db2
sondrev <sonves@gmail.com>
2016-11-07 09:56:00 +0100

    s

--- role.builder.js
+++ role.builder.js
@@ -18,54 +18,30 @@ var roleBuilder = {
           creep.memory.targetFix= tasks.findStructureToRepair(creep);
         }
 
-        if(creep.memory.targetFix.hits<creep.memory.targetFix.hitsMax) {
-            if(creep.repair(Game.getObjectById(creep.memory.targetFix.id)) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(Game.getObjectById(creep.memory.targetFix.id));
-            }
-          } else {
+        if (creep.memory.targetFix===null || creep.memory.targetFix===undefined) {
+          var targetsPri = creep.room.find(FIND_CONSTRUCTION_SITES,{filter: (structure) => { return (
+                          structure.structureType == STRUCTURE_CONTAINER ||
+                          structure.structureType == STRUCTURE_EXTENSION )  } });
+          var targetsAll = creep.room.find(FIND_CONSTRUCTION_SITES);
+            if(targetsPri.length) {
+                if(creep.build(targetsPri[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targetsPri[0]);
+                }
+            } else if (targetsAll.length) {
+              if(creep.build(targetsAll[0]) == ERR_NOT_IN_RANGE) {
+                  creep.moveTo(targetsAll[0]);
+              }
+            } else {
+                creep.say('idle');
+              }
+            } else {
 
-
-        	        var targetsPri = creep.room.find(FIND_CONSTRUCTION_SITES,{
-                          filter: (structure) => {
-                              return (
-                                  structure.structureType == STRUCTURE_CONTAINER ||
-                                  structure.structureType == STRUCTURE_EXTENSION )  }
-                  });
-                  var targetsAll = creep.room.find(FIND_CONSTRUCTION_SITES);
-                  /*
-                  for (var i=0;i<targets.length;i++) {
-                    var t=targets[i];
-                    var lairs = t.pos.findInRange(FIND_STRUCTURES,10, {
-                            filter: (structure) => {
-                                return (
-                                    structure.structureType == STRUCTURE_KEEPER_LAIR && structure.my == false )  }
-                    });
-                    if (lairs.length>0) {
-                      targets.splice(i,1);
-                    }
-                  } */
-                    if(targetsPri.length) {
-                        if(creep.build(targetsPri[0]) == ERR_NOT_IN_RANGE) {
-                            creep.moveTo(targetsPri[0]);
-                        }
-                    } else if (targetsAll.length) {
-                      if(creep.build(targetsAll[0]) == ERR_NOT_IN_RANGE) {
-                          creep.moveTo(targetsAll[0]);
-                      }
-                    } else {
-                        creep.say('idle');
-                        var targets = creep.room.find(FIND_STRUCTURES, {
-                                    filter: (structure) => {
-                                        return (structure.structureType == STRUCTURE_SPAWN);
-                                    }
-                            });
-                            if(targets.length > 0) {
-                                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                                    creep.moveTo(targets[0]);
-                                }
-                        }
             }
-          }
+            if(creep.memory.targetFix.hits<creep.memory.targetFix.hitsMax) {
+                if(creep.repair(Game.getObjectById(creep.memory.targetFix.id)) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(Game.getObjectById(creep.memory.targetFix.id));
+                }
+              }
 	    } else {
         tasks.haulFromContainerAny(creep);
 	    }
