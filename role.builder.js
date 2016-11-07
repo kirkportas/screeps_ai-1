50f1eb568fbc601ba6a20861f58e129d92c058fd
sondrev <sonves@gmail.com>
2016-11-07 10:50:13 +0100

    s

--- role.builder.js
+++ role.builder.js
@@ -36,7 +36,7 @@ var roleBuilder = {
             } else {
               if (Game.getObjectById(creep.memory.targetFix)===null) creep.say('kill');
               //if(Game.getObjectById(creep.memory.targetFix).hits<(Game.getObjectById(creep.memory.targetFix).hitsMax*0.75)) {
-                console.log(Game.getObjectById(creep.memory.targetFix).hits,'  ',Game.getObjectById(creep.memory.targetFix).hitsMax*0.75);
+                //console.log(Game.getObjectById(creep.memory.targetFix).hits,'  ',Game.getObjectById(creep.memory.targetFix).hitsMax*0.75);
                   //if(creep.repair(Game.getObjectById(creep.memory.targetFix)) == ERR_NOT_IN_RANGE) {
                     //  creep.moveTo(Game.getObjectById(creep.memory.targetFix));
                   //}
