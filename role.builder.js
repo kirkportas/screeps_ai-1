16863a74862a06bee2edb7db3f2c28a933107894
sondrev <sonves@gmail.com>
2016-11-07 12:48:19 +0100

    s

--- role.builder.js
+++ role.builder.js
@@ -36,7 +36,7 @@ var roleBuilder = {
               }
             } else {
               var struct=Game.getObjectById(creep.memory.targetFix);
-              if (struct)===null) creep.say('kill');
+              if (struct===null) creep.say('kill');
               if(struct.hits<struct.hitsMax*0.75 && struct.structureType!=STRUCTURE_WALL)) {
                 //console.log(Game.getObjectById(creep.memory.targetFix).hits,'  ',Game.getObjectById(creep.memory.targetFix).hitsMax*0.75);
                   if(creep.repair(Game.getObjectById(creep.memory.targetFix)) == ERR_NOT_IN_RANGE) {
