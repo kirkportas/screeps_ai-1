59c1b92b7f6ac791a2548b0df979cb4231a95a0d
sondrev <sonves@gmail.com>
2016-11-07 12:20:21 +0100

    s

--- main.spawn.js
+++ main.spawn.js
@@ -93,7 +93,7 @@ var mainSpawn = {
     } else  if(builders < buildersNeeded) {
         var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], findNextName('builder'), {role: 'builder'});
         console.log('Spawning new builder: ' + newName);
-    } else if(upgraders < 1 || (centralContainer.store[RESOURCE_ENERGY]>1800 && upgraders<10)) {
+    } else if(upgraders < 1 || (centralContainer.store[RESOURCE_ENERGY]>1000 && upgraders<10)) {
       var newName = Game.spawns['Spawn1'].createCreep([WORK,WORK,CARRY,CARRY,CARRY,CARRY,MOVE,MOVE], findNextName('upgrader'), {role: 'upgrader'});
       console.log('Spawning new upgrader: ' + newName);
     }
