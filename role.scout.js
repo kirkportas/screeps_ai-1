66fab3770c06320b020638e7fab0f9ffef7bae23
sondrev <sonves@gmail.com>
2016-11-07 16:50:47 +0100

    s

--- role.scout.js
+++ role.scout.js
@@ -9,7 +9,7 @@ var roleScout = {
         var exit = creep.pos.findClosestByRange(exitDir);
         creep.moveTo(exit);
       } else {
-        console.log('in other room');
+        console.log('-----------in other room');
         tasks.harvestClosest(creep);
       }
     }
