04074960d0f950985b769de98fe4d7a7a2c287a2
sondrev <sonves@gmail.com>
2016-11-07 09:22:23 +0100

    s

--- build.extension.js
+++ build.extension.js
@@ -4,13 +4,22 @@ var buildExtension = {
 
 
       //innerste runde
+      Game.spawns['Spawn1'].room.createConstructionSite(Game.spawns['Spawn1'].pos.x+1,Game.spawns['Spawn1'].pos.y+1,STRUCTURE_EXTENSION);
+      Game.spawns['Spawn1'].room.createConstructionSite(Game.spawns['Spawn1'].pos.x-1,Game.spawns['Spawn1'].pos.y+1,STRUCTURE_EXTENSION);
+      Game.spawns['Spawn1'].room.createConstructionSite(Game.spawns['Spawn1'].pos.x+1,Game.spawns['Spawn1'].pos.y-1,STRUCTURE_EXTENSION);
+      Game.spawns['Spawn1'].room.createConstructionSite(Game.spawns['Spawn1'].pos.x-1,Game.spawns['Spawn1'].pos.y-1,STRUCTURE_EXTENSION);
+
+      //andre rundep
         Game.spawns['Spawn1'].room.createConstructionSite(Game.spawns['Spawn1'].pos.x+2,Game.spawns['Spawn1'].pos.y+2,STRUCTURE_EXTENSION);
         Game.spawns['Spawn1'].room.createConstructionSite(Game.spawns['Spawn1'].pos.x-2,Game.spawns['Spawn1'].pos.y+2,STRUCTURE_EXTENSION);
         Game.spawns['Spawn1'].room.createConstructionSite(Game.spawns['Spawn1'].pos.x+2,Game.spawns['Spawn1'].pos.y-2,STRUCTURE_EXTENSION);
         Game.spawns['Spawn1'].room.createConstructionSite(Game.spawns['Spawn1'].pos.x-2,Game.spawns['Spawn1'].pos.y-2,STRUCTURE_EXTENSION);
-        //andre rundep
+        /*
         Game.spawns['Spawn1'].room.createConstructionSite(Game.spawns['Spawn1'].pos.x+2,Game.spawns['Spawn1'].pos.y,STRUCTURE_EXTENSION);
-
+        Game.spawns['Spawn1'].room.createConstructionSite(Game.spawns['Spawn1'].pos.x-2,Game.spawns['Spawn1'].pos.y,STRUCTURE_EXTENSION);
+        //Game.spawns['Spawn1'].room.createConstructionSite(Game.spawns['Spawn1'].pos.x,Game.spawns['Spawn1'].pos.y+2,STRUCTURE_EXTENSION); //reservert av cont
+        Game.spawns['Spawn1'].room.createConstructionSite(Game.spawns['Spawn1'].pos.x,Game.spawns['Spawn1'].pos.y-2,STRUCTURE_EXTENSION);
+        */
     }
 
 
