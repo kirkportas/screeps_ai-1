30fa9a739595321d5568c460912bedb1a9fc9779
sondrev <sonves@gmail.com>
2016-11-07 17:04:38 +0100

    s

--- tasks.js
+++ tasks.js
@@ -154,7 +154,7 @@ var tasks = {
     },
 
     harvestClosest: function(creep) {
-      var sources = creep.room.find(FIND_SOURCES);
+      var sources = creep.pos.findClosestByRange(FIND_SOURCES);
       if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
           creep.moveTo(sources[0]);
       }
