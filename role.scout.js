8c01c248b9dd504bf3e727b7a852547837301af3
sondrev <sonves@gmail.com>
2016-11-07 17:52:29 +0100

    s

--- role.scout.js
+++ role.scout.js
@@ -29,7 +29,7 @@ var roleScout = {
           var exit = creep.pos.findClosestByRange(exitDir);
           creep.moveTo(exit);
         } else {
-          var containers = Room[firstRoom].find(FIND_MY_STRUCTURES);
+          var containers = Memory.rooms[firstRoom].find(FIND_MY_STRUCTURES);
           console.log('yaaaaay ',containers.length);
           if (containers.length>0) {
             creep.moveTo(containers[0]);
