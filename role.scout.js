f54e9c7c2ac5feec761161f6b2fec09c9f0c41b0
sondrev <sonves@gmail.com>
2016-11-07 21:20:55 +0100

    s

--- role.scout.js
+++ role.scout.js
@@ -11,29 +11,28 @@ var roleScout = {
       if(!creep.memory.delivering && creep.carry.energy == creep.carryCapacity) {
           creep.memory.delivering = true;
       }
-      var startRoom='E65S62';
-      var targetRoom='E65S61';
-      //creep.say(creep.room.name);
+      //var startRoom='E65S62';
+      //var targetRoom='E65S61';
 
       if(!creep.memory.delivering) {
-        if(creep.room.name != targetRoom) {
-          var exitDir = Game.map.findExit(creep.room, targetRoom);
+        if(creep.room.name != creep.memory.targetRoom) {
+          var exitDir = Game.map.findExit(creep.room, creep.memory.targetRoom);
           var exit = creep.pos.findClosestByRange(exitDir);
           creep.moveTo(exit);
         } else {
           if (creep.memory.target===null || creep.memory.target===undefined) {
-            creep.memory.target = tasks.findBiggestInRoom(creep,Game.rooms[targetRoom]);
+            creep.memory.target = tasks.findBiggestInRoom(creep,Game.rooms[creep.memory.targetRoom]);
           }
-          tasks.harvestBiggestInRoom(creep,Game.rooms[targetRoom],creep.memory.target);
+          tasks.harvestBiggestInRoom(creep,Game.rooms[creep.memory.targetRoom],creep.memory.target);
         }
       } else {
-        if(creep.room.name != startRoom) {
+        if(creep.room.name != creep.memory.startRoom) {
 
-          var exitDir = Game.map.findExit(creep.room, startRoom);
+          var exitDir = Game.map.findExit(creep.room, creep.memory.startRoom);
           var exit = creep.pos.findClosestByRange(exitDir);
           creep.moveTo(exit);
         } else {
-          var containers = Game.rooms[startRoom].find(FIND_MY_STRUCTURES);
+          var containers = Game.rooms[creep.memory.startRoom].find(FIND_MY_STRUCTURES);
           var centralContainer=Game.spawns['Spawn1'].pos.findInRange(FIND_STRUCTURES,5, {
                           filter: (structure) => {
                             return (structure.structureType == STRUCTURE_CONTAINER)
