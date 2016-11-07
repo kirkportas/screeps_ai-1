aba1ee34aec41913bdd499991b2c3a7ae4001aef
sondrev <sonves@gmail.com>
2016-11-07 16:51:52 +0100

    s

--- role.scout.js
+++ role.scout.js
@@ -5,10 +5,12 @@ var roleScout = {
     run: function(creep) {
       var anotherRoomName='E65S61';
       if(creep.room != anotherRoomName) {
+        creep.say('room1');
         var exitDir = Game.map.findExit(creep.room, anotherRoomName);
         var exit = creep.pos.findClosestByRange(exitDir);
         creep.moveTo(exit);
       } else {
+        creep.say('room2');
         console.log('-----------in other room');
         tasks.harvestClosest(creep);
       }
