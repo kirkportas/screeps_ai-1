66338dd1fd96669d2613642ce48a99360e6111d0
sondrev <sonves@gmail.com>
2016-11-08 21:11:15 +0100

    s

--- role.warrior.js
+++ role.warrior.js
@@ -5,11 +5,11 @@ var roleWarrior = {
     run: function(creep) {
 
       var closestHostile = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-      if (closestHostile) [
+      if (closestHostile) {
         if (creep.rangedAttack(closestHostile)==ERR_NOT_IN_RANGE) {
           creep.moveTo(closestHostile);
-        };
-      ]
+        }
+      }
 
     }
 };
