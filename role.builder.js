4f9a837821878553d3ae1255bf977e0738f74ee5
sondrev <sonves@gmail.com>
2016-11-08 22:47:26 +0100

    s

--- role.builder.js
+++ role.builder.js
@@ -36,11 +36,12 @@ var roleBuilder = {
             } else {
                 creep.say('idle');
                 creep.memory.targetFix= tasks.findStructureToRepairIdle(creep);
+                creep.memory.repairToFull=true;
               }
             } else {
               var struct=Game.getObjectById(creep.memory.targetFix);
               if (struct===null) creep.say('kill');
-              if((struct.hits<struct.hitsMax*0.75 && struct.structureType!=STRUCTURE_WALL && struct.structureType!=STRUCTURE_RAMPART) || (struct.hits<creep.room.memory.wallHitsMax && (struct.structureType==STRUCTURE_WALL||struct.structureType==STRUCTURE_RAMPART))) {
+              if((creep.memory.repairToFull) || (struct.hits<struct.hitsMax*0.75 && struct.structureType!=STRUCTURE_WALL && struct.structureType!=STRUCTURE_RAMPART) || (struct.hits<creep.room.memory.wallHitsMax && (struct.structureType==STRUCTURE_WALL||struct.structureType==STRUCTURE_RAMPART))) {
                 //console.log(Game.getObjectById(creep.memory.targetFix).hits,'  ',Game.getObjectById(creep.memory.targetFix).hitsMax*0.75);
                   if(creep.repair(Game.getObjectById(creep.memory.targetFix)) == ERR_NOT_IN_RANGE) {
                       creep.moveTo(Game.getObjectById(creep.memory.targetFix));
@@ -48,6 +49,7 @@ var roleBuilder = {
                 } else {
                   creep.say('reset');
                   creep.memory.targetFix=null;
+                  creep.memory.repairToFull=false;
                 }
             }
 
