36b00ed91ecffd050761f19415b8c5b3d74d11d8
sondrev <sonves@gmail.com>
2016-11-09 16:18:40 +0100

    s

--- main.spawn.js
+++ main.spawn.js
@@ -141,7 +141,7 @@ var mainSpawn = {
   var creep = spawn.pos.findInRange(FIND_CREEPS,1);
   if (creep.length && creep[0].memory.role=='builder' && buildersNeeded==0) {
     spawn.recycleCreep(creep[0]);
-  } else if (creep.length && creep[0].ticksToLive<1200) {
+  } else if (creep.length && creep[0].ticksToLive<500) {
     spawn.renewCreep(creep[0]);
   }
   var hostileSpawn = spawn.pos.findInRange(FIND_HOSTILE_CREEPS,10); //
