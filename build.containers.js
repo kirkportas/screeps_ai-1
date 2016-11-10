244ae9e9df064469ba1ecce50bd8cc0acaecc428
sondrev <sonves@gmail.com>
2016-11-10 12:32:33 +0100

    s

--- build.containers.js
+++ build.containers.js
@@ -1,10 +1,11 @@
 var buildContainers = {
-    run: function() {
+    run: function(room) {
 
-        var posSpawn = new RoomPosition(Game.spawns['Spawn1'].pos.x, Game.spawns['Spawn1'].pos.y+1, Game.spawns['Spawn1'].room.name);
-        Game.spawns['Spawn1'].room.createConstructionSite(Game.spawns['Spawn1'].pos.x, Game.spawns['Spawn1'].pos.y+2,STRUCTURE_CONTAINER)
+        var posSpawn = room.find(FIND_MY_SPAWNS)[0].pos;
+        //var posSpawn = new RoomPosition(Game.spawns['Spawn1'].pos.x, Game.spawns['Spawn1'].pos.y+1, Game.spawns['Spawn1'].room.name);
+        room.createConstructionSite(posSpawn.x+2, posSpawn.y,STRUCTURE_CONTAINER)
 
-        var sources = Game.spawns['Spawn1'].room.memory.allSources;
+        var sources = room.memory.allSources;
         for (var s=0;s<sources.length;s++) {
           var source=sources[s];
           var sourceObj = Game.getObjectById(source.id);
@@ -13,10 +14,10 @@ var buildContainers = {
           var bestSpotPoints=-1;
           for (var x1=-1;x1<2;x1++) { //LOOP though spots around source
             for (var y1=-1;y1<2;y1++) {
-              var items = Game.spawns['Spawn1'].room.lookAt(sourceObj.pos.x+x1,sourceObj.pos.y+y1);
+              var items = room.lookAt(sourceObj.pos.x+x1,sourceObj.pos.y+y1);
               for (let i=0;i<items.length;i++) {
                   if (items[i].terrain=='plain' || items[i].terrain=='swamp') {
-                    var newPos = new RoomPosition(sourceObj.pos.x+x1,sourceObj.pos.y+y1,Game.spawns['Spawn1'].room.name);
+                    var newPos = new RoomPosition(sourceObj.pos.x+x1,sourceObj.pos.y+y1,room.name);
                     if (!spots.includes(newPos)) {spots.push(newPos);}
                   }
               }
@@ -25,7 +26,7 @@ var buildContainers = {
           for (var x1=-2;x1<3;x1++) { //LOOP though spots around source
             for (var y1=-2;y1<3;y1++) {
               var points=0;
-              var newPos = new RoomPosition(sourceObj.pos.x+x1,sourceObj.pos.y+y1,Game.spawns['Spawn1'].room.name);
+              var newPos = new RoomPosition(sourceObj.pos.x+x1,sourceObj.pos.y+y1,room.name);
               //if (spots.includes(newPos)) { continue;}
               var items = Game.spawns['Spawn1'].room.lookAt(sourceObj.pos.x+x1,sourceObj.pos.y+y1);
               for (let i=0;i<items.length;i++) {
@@ -42,11 +43,11 @@ var buildContainers = {
               }
               if (points>bestSpotPoints) {
                 bestSpotPoints=points;
-                bestSpot= new RoomPosition(sourceObj.pos.x+x1,sourceObj.pos.y+y1,Game.spawns['Spawn1'].room.name);
+                bestSpot= new RoomPosition(sourceObj.pos.x+x1,sourceObj.pos.y+y1,room.name);
               }
             }
           }
-          Game.spawns['Spawn1'].room.createConstructionSite(bestSpot.x,bestSpot.y,STRUCTURE_CONTAINER)
+          room.createConstructionSite(bestSpot.x,bestSpot.y,STRUCTURE_CONTAINER)
           //console.log(spots.length);
         }
 
