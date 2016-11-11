a4a67e7e3871be038b473083adb20e22a203909c
sondrev <sonves@gmail.com>
2016-11-11 20:06:12 +0100

    s

--- main.scout.js
+++ main.scout.js
@@ -2,7 +2,7 @@ var tasks = require('tasks');
 var mainScout = {
 
     run: function(room) {
-      if (true||room.memory.scout===undefined || room.memory.scout.length===undefined || room.memory.scout.length===0) {
+      if (room.memory.scout===undefined || room.memory.scout.length===undefined || room.memory.scout.length===0) {
         //console.log('setting basic scout info');
         var exits =Game.map.describeExits(room.name);
         var rooms=[];
