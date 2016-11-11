a38354fa662ef471f7a727a10fe91b841067812b
sondrev <sonves@gmail.com>
2016-11-11 20:26:07 +0100

    s

--- main.spawn.js
+++ main.spawn.js
@@ -160,10 +160,10 @@ var mainSpawn = {
       createCreepAdvanced(spawn,'upgrader',modules);
       //var newName = spawn.createCreep(modules, findNextName('upgrader'), {role: 'upgrader'});
       //console.log('Spawning new upgrader: ' + newName);
-    } else if(scouts < 0) {
-      createCreepAdvanced(spawn,'scout',createBody({move:1}),{targetRoom:'E64S62'});
+    } else if(scouts < 1) {
+      createCreepAdvanced(spawn,'scout',createBody({move:1}),{targetRoom:'E65S63'});
     } else if(attacker < 0) {
-      createCreepAdvanced(spawn,'attacker',createBody({move:1,attack:1}),{targetRoom:'E64S62'});
+      createCreepAdvanced(spawn,'attacker',createBody({move:1,attack:1}),{targetRoom:'E65S63'});
     } else if(warriors < 0) {
       var newName = spawn.createCreep([MOVE,MOVE,RANGED_ATTACK,RANGED_ATTACK,RANGED_ATTACK,RANGED_ATTACK], findNextName('warrior'), {role: 'warrior'});
       console.log('Spawning new warrior: ' + newName);
