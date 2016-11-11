1bd594fb367fd275b2c9422e4fe83992221a6e71
sondrev <sonves@gmail.com>
2016-11-11 11:50:10 +0100

    s

---
+++ role.scoutMiner.js
@@ -0,0 +1,60 @@
+var tasks = require('tasks');
+
+var roleScout = {
+
+    run: function(creep) {
+      if(creep.memory.delivering && creep.carry.energy == 0) {
+            creep.memory.delivering = false;
+            creep.memory.delivered+=creep.carryCapacity;
+            creep.memory.target=null;
+      }
+      if(!creep.memory.delivering && creep.carry.energy == creep.carryCapacity) {
+          creep.memory.delivering = true;
+      }
+      //var startRoom='E65S62';
+      //var targetRoom='E65S61';
+
+      if(!creep.memory.delivering) {
+        if(creep.room.name != creep.memory.targetRoom) {
+          var exitDir = Game.map.findExit(creep.room, creep.memory.targetRoom);
+          var exit = creep.pos.findClosestByRange(exitDir);
+          creep.moveTo(exit);
+          /*
+          var value = creep.moveTo(Game.getObjectById(creep.memory.pref));
+          if (value == ERR_INVALID_TARGET || value== ERR_NO_PATH) {
+            var exitDir = Game.map.findExit(creep.room, creep.memory.targetRoom);
+            var exit = creep.pos.findClosestByRange(exitDir);
+            creep.moveTo(exit);
+          } */
+
+        } else {
+          if (creep.memory.target===null || creep.memory.target===undefined) {
+            creep.memory.target = tasks.findBiggestInRoom(creep,Game.rooms[creep.memory.targetRoom]);
+          }
+          //tasks.harvestPrefered(creep);
+          tasks.harvestBiggestInRoom(creep,Game.rooms[creep.memory.targetRoom],creep.memory.target);
+        }
+      } else {
+        if(creep.room.name != creep.memory.startRoom) {
+          var exitDir = Game.map.findExit(creep.room, creep.memory.startRoom);
+          var exit = creep.pos.findClosestByRange(exitDir);
+          creep.moveTo(exit);
+        } else {
+          var containers = Game.rooms[creep.memory.startRoom].find(FIND_MY_STRUCTURES);
+          var centralContainer=Game.spawns['Spawn1'].pos.findInRange(FIND_STRUCTURES,8, {
+                          filter: (structure) => {
+                            return (structure.structureType == STRUCTURE_STORAGE)
+                        }})[0];
+          if(creep.transfer(centralContainer, RESOURCE_ENERGY)== ERR_NOT_IN_RANGE) {
+              creep.moveTo(centralContainer);
+          }
+        }
+
+
+      }
+
+
+    }
+};
+
+module.exports = roleScout;
