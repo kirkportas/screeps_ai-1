dd14744d18b364f53e0d7ccc7988b2d27e698132
sondrev <sonves@gmail.com>
2016-11-11 19:50:10 +0100

    s

--- role.scout.js
+++ role.scout.js
@@ -8,7 +8,7 @@ var roleScout = {
         var exit = creep.pos.findClosestByRange(exitDir);
         creep.moveTo(exit);
       } else {
-        if (creep.homeRoom.memory.scout[creep.memory.targetRoom]) {
+        if (creep.memory.homeRoom.memory.scout[creep.memory.targetRoom]) {
           console.log('exists');
 
         }
