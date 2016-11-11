f89605f2f9264dca04911174557f1b542c76833c
sondrev <sonves@gmail.com>
2016-11-11 17:45:57 +0100

    s

--- main.scout.js
+++ main.scout.js
@@ -5,11 +5,16 @@ var mainScout = {
       if (true||room.memory.scout===undefined || room.memory.scout.length===undefined || room.memory.scout.length===0) {
         //console.log('setting basic scout info');
         var exits =Game.map.describeExits(room.name);
-        console.log(exits[1]);
-        if (exits[3] != undefined) console.log(exits[3]);
-        console.log(exits[5]);
-        console.log(exits[7]);
-        room.memory.scout=exits;
+        var rooms=[];
+        if (exits[1] != undefined) rooms.push(exits[1]);
+        if (exits[3] != undefined) rooms.push(exits[3]);
+        if (exits[5] != undefined) rooms.push(exits[5]);
+        if (exits[7] != undefined) rooms.push(exits[7]);
+        room.memory.scout=[];
+        for (var i=0;i<rooms.length;i++) {
+          room.memory.scout.push({roomname:rooms[i],timeSinceLastScout:0, danger:0,sources:[]});
+        }
+
 
       }
 
