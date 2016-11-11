d602e253ad68bc0ffea5378e7cb14ce71aae5d5f
sondrev <sonves@gmail.com>
2016-11-11 18:18:00 +0100

    s

--- main.scout.js
+++ main.scout.js
@@ -12,7 +12,7 @@ var mainScout = {
         if (exits[7] != undefined) rooms.push(exits[7]);
         room.memory.scout={}
         for (var i=0;i<rooms.length;i++) {
-          room.memory.scout.rooms.test=1;
+          //room.memory.scout.rooms.test=1;
 
           //room.memory.scout.push({roomname:rooms[i],timeSinceLastScout:0, danger:0,sources:[]});
         }
