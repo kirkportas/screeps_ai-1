c5b9a37d624ad6c795084b590b9db61fc3db20cd
sondrev <sonves@gmail.com>
2016-11-11 17:18:15 +0100

    s

---
+++ main.scout.js
@@ -0,0 +1,16 @@
+var tasks = require('tasks');
+var mainScout = {
+
+    run: function(room) {
+      if (true||room.memory.scout===undefined || room.memory.scout.length===undefined || room.memory.scout.length===0) {
+        console.log('setting basic scout info');
+        room.memory.scout=Map.describeExits(room.name);
+      }
+
+
+
+
+     }
+
+};
+module.exports = mainScout;
