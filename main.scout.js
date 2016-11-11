8896c95e5f98dfbc64968d4dc8ff0a401d9b966c
sondrev <sonves@gmail.com>
2016-11-11 17:38:23 +0100

    s

--- main.scout.js
+++ main.scout.js
@@ -5,10 +5,7 @@ var mainScout = {
       if (true||room.memory.scout===undefined || room.memory.scout.length===undefined || room.memory.scout.length===0) {
         //console.log('setting basic scout info');
         var exits =Game.map.describeExits(room.name);
-        console.log(exits[1].name);
-        //if (exits[3] != undefined) console.log(exits[3].name);
-        //console.log(exits[5].name);
-        //console.log(exits[7].name);
+        console.log(exits[1]);
         room.memory.scout=exits;
 
       }
