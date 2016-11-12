2ff1f52ecf0c17c72d10f2c5c570b5e7329433e2
sondrev <sonves@gmail.com>
2016-11-12 13:04:00 +0100

    s

--- role.upgrader.js
+++ role.upgrader.js
@@ -12,9 +12,11 @@ var roleUpgrader = {
 
       // OPPGRADERINGSMODUS
 	    if(creep.memory.upgrading) {
+        /*
         if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
             creep.moveTo(creep.room.controller);
         }
+        */
       }
       //HENTEMODUS
       if(!creep.memory.upgrading) {
