c4779dc3d87aa8007281e0ee4ccd3094602b5af7
sondrev <sonves@gmail.com>
2016-11-12 04:42:47 +0100

    s

--- role.builder.js
+++ role.builder.js
@@ -19,8 +19,6 @@ var roleBuilder = {
 
         if (creep.memory.targetFix===null || creep.memory.targetFix===undefined) {
           var targetsPri = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES,{filter: (structure) => { return (
-                          structure.structureType == STRUCTURE_WALL ||
-                          structure.structureType == STRUCTURE_RAMPART ||
                           structure.structureType == STRUCTURE_TOWER ||
                           structure.structureType == STRUCTURE_STORAGE ||
                           structure.structureType == STRUCTURE_LINK ||
