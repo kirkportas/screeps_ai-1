a064ccc0e418b8755a15ecd4ed3d4aebbb394ae6
sondrev <sonves@gmail.com>
2016-11-12 13:22:06 +0100

    s

--- main.spawn.js
+++ main.spawn.js
@@ -95,7 +95,9 @@ var mainSpawn = {
     }
 
     var upgradersNeeded = 1;
-    if (centralContainer.store[RESOURCE_ENERGY]>centralContainer.storeCapacity*0.75) upgradersNeeded+=3;
+    if (centralContainer.store[RESOURCE_ENERGY]>centralContainer.storeCapacity*0.50) upgradersNeeded+=1;
+    if (centralContainer.store[RESOURCE_ENERGY]>centralContainer.storeCapacity*0.75) upgradersNeeded+=1;
+    if (centralContainer.store[RESOURCE_ENERGY]==centralContainer.storeCapacity) upgradersNeeded+=1;
     upgradersNeeded+=Math.max(0,Math.floor((centralContainer.store[RESOURCE_ENERGY]-10000)/10000));
     console.log('builders needed',buildersNeeded)
     //  || ((centralContainer.store[RESOURCE_ENERGY]>centralContainer.storeCapacity*0.75 || centralContainer.store[RESOURCE_ENERGY]>20000) && upgraders<4
