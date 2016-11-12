3d5ab5dd8718bd76b197fa19475b16ef5fcac4d1
sondrev <sonves@gmail.com>
2016-11-12 12:58:24 +0100

    s

--- main.spawn.js
+++ main.spawn.js
@@ -180,7 +180,7 @@ var mainSpawn = {
   }
   var hostileSpawn = spawn.pos.findInRange(FIND_HOSTILE_CREEPS,10); //
   var hostileConstroller = spawn.room.controller.pos.findInRange(FIND_HOSTILE_CREEPS,10);
-  if (hostileSpawn.length||hostileConstroller.length) {
+  if (spawn.room.name=='E65S62' && (hostileSpawn.length||hostileConstroller.length)) {
     var value = spawn.room.controller.activateSafeMode();
     console.log('WARNING - ENEMY IN BASE - safemode activated: '+value);
     Game.notify('WARNING - ENEMY IN BASE - safemode activated: '+value);
