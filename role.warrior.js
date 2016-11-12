b1a811fc9c08ea027666822f883b4f27bbb2a38a
sondrev <sonves@gmail.com>
2016-11-12 12:33:08 +0100

    s

--- role.warrior.js
+++ role.warrior.js
@@ -3,6 +3,7 @@ var tasks = require('tasks');
 var roleWarrior = {
 
     run: function(creep) {
+      creep.say('t')
       var targets = creep.pos.findInRange(FIND_HOSTILE_CREEPS, 3);
       if(targets.length > 0) {
           creep.rangedAttack(targets[0]);
