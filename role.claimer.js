e4c57c245c7da5250604882532d1515fad1dfca4
sondrev <sonves@gmail.com>
2016-11-12 03:27:09 +0100

    s

--- role.claimer.js
+++ role.claimer.js
@@ -8,7 +8,11 @@ var roleClaimer = {
         var exit = creep.pos.findClosestByRange(exitDir);
         creep.moveTo(exit);
       } else {
+        /*
         if (creep.claimController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(creep.room.controller);
+        }  */
+        if (creep.reserveController(creep.room.controller) == ERR_NOT_IN_RANGE) {
           creep.moveTo(creep.room.controller);
         }
 
