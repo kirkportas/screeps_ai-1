18b2c86195affa5969f531855f546c817e725277
sondrev <sonves@gmail.com>
2016-11-13 22:38:57 +0100

    s

--- role.claimer.js
+++ role.claimer.js
@@ -10,13 +10,13 @@ var roleClaimer = {
       } else {
         var controllerId = Game.rooms[creep.memory.targetRoom].controller.id;
         console.log(controllerId);
-        Game.getObjectById(controllerId);
+        var controller = Game.getObjectById(controllerId);
         /*
         if (creep.claimController(creep.room.controller) == ERR_NOT_IN_RANGE) {
           creep.moveTo(creep.room.controller);
         }  */
-        if (creep.reserveController(Game.rooms[creep.memory.targetRoom].controller) == ERR_NOT_IN_RANGE) {
-          console.log(creep.moveTo(Game.rooms[creep.memory.targetRoom].controller));
+        if (creep.reserveController(controller) == ERR_NOT_IN_RANGE) {
+          console.log(creep.moveTo(controller));
         }
 
       }
