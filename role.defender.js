44e7416fd9c0fc59c24f181ea144577f7d7de808
sondrev <sonves@gmail.com>
2016-11-13 02:26:53 +0100

    s

--- role.defender.js
+++ role.defender.js
@@ -30,7 +30,7 @@ var roleDefender = {
         if (creep.rangedAttack(targetClosest) == ERR_NOT_IN_RANGE) {
           creep.moveTo(targetClosest);
         }
-      } else if (creep.memory.spawnerAction.localeCompare('KILL')) {
+      } else if (creep.memory.spawnerAction=='kill') {
         creep.say('ya')
           var spawn=creep.room.find(FIND_MY_SPAWNS)[0];
           if (spawn) {
