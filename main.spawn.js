f293b00a2140d2dd99e2c11b21f1aec843405db0
sondrev <sonves@gmail.com>
2016-11-13 05:15:04 +0100

    s

--- main.spawn.js
+++ main.spawn.js
@@ -183,7 +183,7 @@ var mainSpawn = {
       createCreepAdvanced(spawn,'scout',createBody({move:1}),{targetRoom:'E64S61'});
     } else if(spawn.room.name=='E65S62' && claimers < 0) {
       createCreepAdvanced(spawn,'claimer',createBody({move:2,claim:2}),{targetRoom:'E64S61'});
-    } else if(spawn.room.name == 'E65S61' && attacker < 1) {
+    } else if(spawn.room.name == 'E65S61' && attacker < 2) {
       createCreepAdvanced(spawn,'attacker',createBody({move:4,attack:4}),{targetRoom:'E64S61'});
     } else if(spawn.room.name=='E65S62' && remoteBuilders < 0) {
       createCreepAdvanced(spawn,'remoteBuilder',createBody({move:5,carry:5,work:5}),{targetRoom:'E64S62'});
