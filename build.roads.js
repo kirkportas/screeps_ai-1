95947fc1743d9c68e1fd831dbd920b242b5e61bb
sondrev <sonves@gmail.com>
2016-11-13 13:38:53 +0100

    s

--- build.roads.js
+++ build.roads.js
@@ -13,7 +13,7 @@ var buildRoads = {
           // Favor roads over plain tiles
           costs.set(structure.pos.x, structure.pos.y, 1);
         } else if (structure.structureType == STRUCTURE_WALL) {
-          console.log('fond wall')
+          //console.log('fond wall')
           // Can't walk through non-walkable buildings
           costs.set(structure.pos.x, structure.pos.y, 255);
         }
