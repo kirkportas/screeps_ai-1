6c1c17e9068cbb35024eb874801413c8111775cc
sondrev <sonves@gmail.com>
2016-11-13 02:11:29 +0100

    s

--- role.defender.js
+++ role.defender.js
@@ -32,7 +32,7 @@ var roleDefender = {
         if (creep.rangedAttack(targetClosest) == ERR_NOT_IN_RANGE) {
           creep.moveTo(targetClosest);
         }
-      } else if (flag!=undefined) {
+      } else if (flag!=undefined && flag.pos.room.name==creep.memory.homeRoom) {
         creep.moveTo(flag.pos);
       } else {
         creep.moveTo(creep.room.controller);
