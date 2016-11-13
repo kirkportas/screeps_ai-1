2a28530b863ba8eb6ec9c7303a97037a6fd2d609
sondrev <sonves@gmail.com>
2016-11-13 01:24:12 +0100

    s

--- role.defender.js
+++ role.defender.js
@@ -3,11 +3,29 @@ var tasks = require('tasks');
 var roleDefender = {
 
     run: function(creep) {
-      
+
+      /*
       var targets = creep.pos.findInRange(FIND_HOSTILE_CREEPS, 3);
       if(targets.length > 0) {
           allHostiles=_.sortBy(targets, t => t.hits);
           creep.rangedAttack(targets[0]);
+        } */
+
+        var targetClosest = creep.pos.findClosestByPath(FIND_HOSTILE_CREEPS);
+        var targetsLowestHits = (creep.room.find(FIND_HOSTILE_CREEPS));
+        //MASS ATTACK CALC
+        var targets1 = (creep.pos.findInRange(FIND_HOSTILE_CREEPS,1).length);
+        var targets2 = (creep.pos.findInRange(FIND_HOSTILE_CREEPS,2).length)-targets1;
+        var targets3 = (creep.pos.findInRange(FIND_HOSTILE_CREEPS,3).length)-targets2-targets1;
+        var possibleDmg= targets1*10+targets2*4+targets1*1;
+        console.log('pos dmg',possibleDmg);
+
+        if (possibleDmg>10) {
+          creep.rangedMassAttack();
+        } else  if (targetClosest) {
+        if (creep.rangedAttack(targetClosest) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(targetClosest);
+        }
       } else {
         var flag = Game.flags['Flag1'];
         creep.moveTo(flag.pos);
