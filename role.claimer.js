38b01a1ea9e72fa7cc4503bc1b2d791aa31839e1
sondrev <sonves@gmail.com>
2016-11-13 22:28:53 +0100

    s

--- role.claimer.js
+++ role.claimer.js
@@ -13,7 +13,7 @@ var roleClaimer = {
           creep.moveTo(creep.room.controller);
         }  */
         if (creep.reserveController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(creep.room.controller);
+          console.log(creep.moveTo(creep.room.controller));
         }
 
       }
