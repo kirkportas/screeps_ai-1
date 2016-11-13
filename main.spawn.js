7721b2de45bd5e8f9d552d61d5376c73df13b359
sondrev <sonves@gmail.com>
2016-11-13 04:32:51 +0100

    s

--- main.spawn.js
+++ main.spawn.js
@@ -99,7 +99,7 @@ var mainSpawn = {
     //console.log(constructionSites.length,' sites need energy: ', energyNeeded,' by builders: ',buildersNeeded,'. Damage to repair: ',repairNeeded);
 
     if (spawn.room.name=='E65S62') {
-      buildersNeeded=1;
+      //buildersNeeded=1;
       haulersNeeded=Math.min(haulersNeeded,2);
     }
 
