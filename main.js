7d4543be8653128cb12fba07576abdcfdeb9fa87
sondrev <sonves@gmail.com>
2016-11-14 11:09:34 +0100

    s

--- main.js
+++ main.js
@@ -76,7 +76,7 @@ module.exports.loop = function () {
     Memory.test3=timeData;
     for (var role in timeData) {
       console.log(role);
-      var avg,tot,n=0;
+      var avg,tot=0,n=0;
       console.log(timeData[role].length)
       for (let i=0;i<timeData[role].length;i++) {
         console.log(timeData[role][i]);
