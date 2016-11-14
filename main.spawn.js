91eaedd1356a235cf96e2e461aba097064257ef8
sondrev <sonves@gmail.com>
2016-11-14 10:00:01 +0100

    s

--- main.spawn.js
+++ main.spawn.js
@@ -44,7 +44,7 @@ var mainSpawn = {
           for (var sourceId in sources) {
             let harvestersRemote = _.filter(Game.creeps, (creep) => creep.memory.homeRoom == spawn.room.name && creep.memory.targetRoom == roomName && creep.memory.role == 'remoteHarvester' && creep.memory.pref == sourceId).length;
             if (harvestersRemote<2) {
-              var size = Math.ceil((spawn.room.energyCapacityAvailable/2)/200);
+              var size = Math.floor((spawn.room.energyCapacityAvailable*0.75)/200);
               createCreepAdvanced(spawn,'remoteHarvester',createBody({move:size,carry:size,work:size}),{targetRoom:roomName, pref: sourceId});
               return true;
             }
