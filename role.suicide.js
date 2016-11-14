12c88519f0048814caf659ffdc0b95f51ee4e00e
sondrev <sonves@gmail.com>
2016-11-14 02:52:18 +0100

    s

--- role.suicide.js
+++ role.suicide.js
@@ -7,6 +7,7 @@ var roleSuicide = {
       var spawn= Game.rooms[creep.memory.homeRoom].find(FIND_MY_SPAWNS)[0];
 
       if(creep.room.name != creep.memory.homeRoom) {
+        creep.say('1')
         var exitDir = Game.map.findExit(creep.room, creep.memory.homeRoom);
         var exit = creep.pos.findClosestByRange(exitDir);
         creep.moveTo(exit);
