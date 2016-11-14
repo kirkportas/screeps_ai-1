00edcd64b58edea184489d89e6f9d01e8194e7a0
sondrev <sonves@gmail.com>
2016-11-14 13:44:45 +0100

    s

--- tasks.js
+++ tasks.js
@@ -61,7 +61,7 @@ var tasks = {
           var spawn= Game.rooms[creep.memory.homeRoom].find(FIND_MY_SPAWNS)[0];
           if (spawn) {
             for (var source in scout.sources) {
-              var path = new PathFinder.search(spawn.pos,{pos:Game.getObjectById(scout.sources[source]).pos,range:1},{plainCost: 1,swampCost: 1});
+              var path = new PathFinder.search(spawn.pos,{pos:Game.getObjectById(source).pos,range:1},{plainCost: 1,swampCost: 1});
               if (path) {
                 Memory.rooms[creep.memory.homeRoom].scout[creep.memory.targetRoom].sources[source].pathLen=path.path.length;
               }
