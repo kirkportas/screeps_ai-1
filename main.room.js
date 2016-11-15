e7b2c85bc9ead77e87a4cb994aa22bea13989ffa
sondrev <sonves@gmail.com>
2016-11-15 20:10:02 +0100

    s

--- main.room.js
+++ main.room.js
@@ -1,7 +1,7 @@
 var buildExtension = require('build.extension');
 var buildContainers = require('build.containers');
 var buildRoads = require('build.roads');
-var roomLinks = require('links');
+//var roomLinks = require('links');
 
 var mainRoom = {
 
