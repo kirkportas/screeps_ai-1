e36f1ef5b09e38112e4e5333b8fee0167f1eb4a6
sondrev <sonves@gmail.com>
2016-11-15 02:22:15 +0100

    s

--- role.claimer.js
+++ role.claimer.js
@@ -4,8 +4,11 @@ var roleClaimer = {
     /** @param {Creep} creep **/
     run: function(creep) {
       if(creep.room.name != creep.memory.targetRoom) {
-        var res = creep.moveTo(Game.rooms[creep.memory.targetRoom].controller);
-        if (res == ERR_INVALID_TARGET||res==ERR_NO_PATH) {
+        var res=null;
+        if (Game.rooms[creep.memory.targetRoom]) {
+          res = creep.moveTo(Game.rooms[creep.memory.targetRoom].controller);
+        }
+        if (res == ERR_INVALID_TARGET||res==ERR_NO_PATH||res==null) {
           var exitDir = Game.map.findExit(creep.room, creep.memory.targetRoom);
           var exit = creep.pos.findClosestByRange(exitDir);
           creep.moveTo(exit);
