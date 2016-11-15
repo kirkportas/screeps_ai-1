55a15fd77d108718059fe9cc4187fbd804a9387e
sondrev <sonves@gmail.com>
2016-11-15 20:47:05 +0100

    s

--- room.links.js
+++ room.links.js
@@ -1,6 +1,16 @@
 var roomLinks = {
   run: function(room) {
-    console.log('yaaay');
+    var sources = room.find(FIND_SOURCES);
+    var linksSpawner =[]
+    _.forEach(structures, function(struc){
+      var link=pos.findInRange(FIND_STRUCTURES,3,{filter: { structureType: STRUCTURE_LINK }})[0]
+      if (link) linksSpawner.push(struc)
+    });
+
+    var linkCentral = room.find(FIND_MY_SPAWNS)[0].pos.findInRange(FIND_STRUCTURES,5,{filter: { structureType: STRUCTURE_LINK }})[0]
+    var linkUpgrader = room.controller.pos.findInRange(FIND_STRUCTURES,3,{filter: { structureType: STRUCTURE_LINK }})[0]
+
+    console.log(linksSpawner+' '+linkCentral+' '+linkUpgrader)
   }
 };
 module.exports = roomLinks;
