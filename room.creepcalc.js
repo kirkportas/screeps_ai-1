0693bbd1661975cb7d0aa152801c03489b9b8afb
sondrev <sonves@gmail.com>
2016-11-16 01:49:55 +0100

    s

--- room.creepcalc.js
+++ room.creepcalc.js
@@ -1,16 +1,16 @@
 var roomCreepcalc = {
   creepCount: function(room) {
     var count = {};
-    count.harvesters = _.filter(Game.creeps, (creep)    => creep.memory.homeRoom == spawn.room.name && creep.memory.role == 'harvester' && creep.ticksToLive>50).length;
-    count.haulers = _.filter(Game.creeps, (creep)       => creep.memory.homeRoom == spawn.room.name && creep.memory.role == 'hauler' && creep.ticksToLive>50).length;
-    count.spawnHaulers = _.filter(Game.creeps, (creep)  => creep.memory.homeRoom == spawn.room.name && creep.memory.role == 'spawnHauler').length;
-    count.upgraders = _.filter(Game.creeps, (creep)     => creep.memory.homeRoom == spawn.room.name && creep.memory.role == 'upgrader'&& creep.ticksToLive>50).length;
-    count.builders = _.filter(Game.creeps, (creep)      => creep.memory.homeRoom == spawn.room.name && creep.memory.role == 'builder'&& creep.ticksToLive>10).length;
-    count.scouts = _.filter(Game.creeps, (creep)        => creep.memory.homeRoom == spawn.room.name && creep.memory.role == 'scout'&&  creep.ticksToLive>50).length;
-    count.defenders = _.filter(Game.creeps, (creep)      => creep.memory.homeRoom == spawn.room.name && creep.memory.role == 'defender').length;
-    count.attacker = _.filter(Game.creeps, (creep)      => creep.memory.homeRoom == spawn.room.name && creep.memory.role == 'attacker').length;
-    count.claimers = _.filter(Game.creeps, (creep)      => creep.memory.homeRoom == spawn.room.name && creep.memory.role == 'claimer').length;
-    count.remoteBuilders = _.filter(Game.creeps, (creep) => creep.memory.homeRoom == spawn.room.name && creep.memory.role == 'remoteBuilder').length;
+    count.harvesters = _.filter(Game.creeps, (creep)    => creep.memory.homeRoom == room.name && creep.memory.role == 'harvester' && creep.ticksToLive>50).length;
+    count.haulers = _.filter(Game.creeps, (creep)       => creep.memory.homeRoom == room.name && creep.memory.role == 'hauler' && creep.ticksToLive>50).length;
+    count.spawnHaulers = _.filter(Game.creeps, (creep)  => creep.memory.homeRoom == room.name && creep.memory.role == 'spawnHauler').length;
+    count.upgraders = _.filter(Game.creeps, (creep)     => creep.memory.homeRoom == room.name && creep.memory.role == 'upgrader'&& creep.ticksToLive>50).length;
+    count.builders = _.filter(Game.creeps, (creep)      => creep.memory.homeRoom == room.name && creep.memory.role == 'builder'&& creep.ticksToLive>10).length;
+    count.scouts = _.filter(Game.creeps, (creep)        => creep.memory.homeRoom == room.name && creep.memory.role == 'scout'&&  creep.ticksToLive>50).length;
+    count.defenders = _.filter(Game.creeps, (creep)      => creep.memory.homeRoom == room.name && creep.memory.role == 'defender').length;
+    count.attacker = _.filter(Game.creeps, (creep)      => creep.memory.homeRoom == room.name && creep.memory.role == 'attacker').length;
+    count.claimers = _.filter(Game.creeps, (creep)      => creep.memory.homeRoom == room.name && creep.memory.role == 'claimer').length;
+    count.remoteBuilders = _.filter(Game.creeps, (creep) => creep.memory.homeRoom == room.name && creep.memory.role == 'remoteBuilder').length;
     return count;
   }
 };
