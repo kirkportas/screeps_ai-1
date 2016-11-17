9640c5dd32f75737fc8cc3a94ab891f652d24fb8
sondrev <sonves@gmail.com>
2016-11-17 23:21:32 +0100

    s

--- build.walls.js
+++ build.walls.js
@@ -4,7 +4,7 @@ var buildWalls = {
 
       var wallW = room.find(FIND_EXIT_BOTTOM);
       for (let i=0;i<wallW.length-1;i++) {
-        if (wallW[i].pos.x+1!=wallW[i+1].pos.x) console.log('new seg');
+        if (wallW[i].x+1!=wallW[i+1].x) console.log('new seg');
       }
     }
 };
