2177c216045e7fd70141f0c955571d0bc0d9abe4
sondrev <sonves@gmail.com>
2016-11-17 23:12:07 +0100

    s

--- build.walls.js
+++ build.walls.js
@@ -2,7 +2,7 @@ var buildWalls = {
 
     run: function(room) {
 
-      var wallW = room.find(FIND_STRUCTURES, {filter: (w) => {return (w.x==1)}});
+      var wallW = room.find(FIND_STRUCTURES, {filter: (w) => {return (w.x==0)}});
       console.log(wallW.length)
     }
 };
