55ef9d5d21eee65e467507a938ffde7cadcf1479
sondrev <sonves@gmail.com>
2016-11-17 23:53:16 +0100

    s

--- build.walls.js
+++ build.walls.js
@@ -1,15 +1,15 @@
 var buildWalls = {
 
-  buildWall: function(x1,x2) {
-    console.log('seg')
-  },
+    buildWall: function(x1,x2) {
+      console.log('seg')
+    },
 
     run: function(room) {
 
       var wallW = room.find(FIND_EXIT_BOTTOM);
       for (let i=0;i<wallW.length-1;i++) {
         console.log(i,' ',wallW[i].x)
-        if (wallW[i].x+1!=wallW[i+1].x) buildWall(,wallW[i].x)
+        if (wallW[i].x+1!=wallW[i+1].x) buildWall(0,wallW[i].x)
       }
     }
 };
