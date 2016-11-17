8f43d1a983b568a11edae4b9303f00c323433962
sondrev <sonves@gmail.com>
2016-11-17 02:50:49 +0100

    s

--- tasks.js
+++ tasks.js
@@ -155,7 +155,7 @@ var tasks = {
        target = target.concat(centralLink);
 
       target = target.concat(spawnTar);
-      if (extensions) target.push(extensions);
+      if (extensions) target=target.push(extensions);
       target = target.concat(tower);
       target = target.concat(centralStorage);
 
