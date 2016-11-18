5eeafcdd9c360d96ae36dcab8a3c56adb06d0561
sondrev <sonves@gmail.com>
2016-11-18 04:27:02 +0100

    s

--- role.remoteBuilder.js
+++ role.remoteBuilder.js
@@ -13,11 +13,11 @@ var roleRemotebuilder = {
 	    }
 
       if(creep.room.name != creep.memory.targetRoom) {
-        creep.moveTo(Game.flags['Flag2'].pos);
+        //creep.moveTo(Game.flags['Flag2'].pos);
 
-        //var exitDir = Game.map.findExit(creep.room, creep.memory.targetRoom);
-        //var exit = creep.pos.findClosestByRange(exitDir);
-        //creep.moveTo(exit);
+        var exitDir = Game.map.findExit(creep.room, creep.memory.targetRoom);
+        var exit = creep.pos.findClosestByRange(exitDir);
+        creep.moveTo(exit);
       } else {
         if(creep.memory.building) {
 
