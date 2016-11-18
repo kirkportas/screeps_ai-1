569d01960d86edc8af68532643583a078acf8243
sondrev <sonves@gmail.com>
2016-11-18 02:58:09 +0100

    s

--- role.remoteBuilder.js
+++ role.remoteBuilder.js
@@ -14,7 +14,7 @@ var roleRemotebuilder = {
       if(creep.room.name != creep.memory.targetRoom) {
         var exitDir = Game.map.findExit(creep.room, creep.memory.targetRoom);
         var exit = creep.pos.findClosestByRange(exitDir);
-        creep.moveTo(exit,{reusePath: 20});
+        creep.moveTo(exit,{reusePath: 1});
       } else {
         if(creep.memory.building) {
 
