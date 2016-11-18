c99ed51bbe79ec87c130d406747550d9973d9326
sondrev <sonves@gmail.com>
2016-11-18 01:19:36 +0100

    s

--- main.js
+++ main.js
@@ -44,8 +44,8 @@ module.exports.loop = function () {
 
   }
   //createCreepAdvanced(Game.spawns['Spawn1'],'attacker',createBody({move:2,attack:1}),{targetRoom:'E68S62'});
-  //createCreepAdvanced(Game.spawns['Spawn1'],'claimer',createBody({move:2,claim:2}),{targetRoom:'E68S62',takeover: true});
-  createCreepAdvanced(Game.spawns['Spawn1'],'remoteBuilder',createBody({move:8,carry:4,work:4}),{targetRoom:'E68S62'});
+  createCreepAdvanced(Game.spawns['Spawn2'],'claimer',createBody({move:2,claim:2}),{targetRoom:'E68S62',takeover: true});
+  //createCreepAdvanced(Game.spawns['Spawn1'],'remoteBuilder',createBody({move:8,carry:4,work:4}),{targetRoom:'E68S62'});
 
   if (cpuLog && !Memory.timeData) {Memory.timeData={}}
   var timeHarvester=0;
