6baf4688670d8fa575c19214269f0e76514d6c0a
sondrev <sonves@gmail.com>
2016-11-18 03:20:31 +0100

    s

--- role.remoteBuilder.js
+++ role.remoteBuilder.js
@@ -12,7 +12,7 @@ var roleRemotebuilder = {
 	    }
 
       if(creep.room.name != creep.memory.targetRoom) {
-        var exitDir = Game.map.findExit(creep.room, creep.memory.targetRoom);
+        var exitDir = Game.map.findExit(creep.room, creep.memory.targetRoom,{maxRooms:1});
         var exit = creep.pos.findClosestByRange(exitDir);
         creep.moveTo(exit);
       } else {
