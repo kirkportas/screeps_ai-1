ecc7b192d24128d3e58c0cf575381ad60b534cd9
sondrev <sonves@gmail.com>
2016-11-20 00:40:06 +0100

    s

--- build.roads.js
+++ build.roads.js
@@ -61,7 +61,7 @@ var buildRoads = {
         //BYGG VEI TIL sources
         var sources = room.memory.allSources;
         for (var i=0;i<sources.length;i++) {
-          buildRoads.buildRoad(posSpawn,Game.getObjectById(sources[i].id).pos);
+          buildRoads.buildDoubleRoad(posSpawn,Game.getObjectById(sources[i].id).pos);
         }
 
         buildRoads.buildDoubleRoad(posSpawn,posCtr);
