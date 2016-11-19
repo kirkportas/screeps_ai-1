1c016ac77e33625bf74f2fcb3b59b3dbbb2d44fc
sondrev <sonves@gmail.com>
2016-11-20 00:03:47 +0100

    s

--- build.roads.js
+++ build.roads.js
@@ -38,7 +38,7 @@ var buildRoads = {
         //BYGG VEI TIL sources
         var sources = room.memory.allSources;
         for (var i=0;i<sources.length;i++) {
-          buildRoad(posSpawn,(Game.getObjectById(sources[i]).pos)
+          buildRoad(posSpawn,(Game.getObjectById(sources[i]).pos));
         }
 
         //BYGG VEI TIL KONTROLLER
