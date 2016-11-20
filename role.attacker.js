3e9d943a75882d5854c1e9d22f7f0a5b5755c665
sondrev <sonves@gmail.com>
2016-11-20 19:33:56 +0100

    s

--- role.attacker.js
+++ role.attacker.js
@@ -46,7 +46,8 @@ var roleAttacker = {
 
     },
     heal: function(creep) {
-      var target = creep.pos.findClosestByRange(FIND_MY_CREEPS, {filter: function(object) {return object.hits < object.hitsMax;}});
+      var target = creep.pos.findClosestByRange(FIND_MY_CREEPS, {filter: function(object) {return object.hits < object.hitsMax}});
+      var leader = creep.pos.findClosestByRange(FIND_MY_CREEPS, {filter: function(object) {return creep.getActiveBodyparts(ATTACK)>0}});
       if(target) {
           creep.moveTo(target);
           if(creep.pos.isNearTo(target)) {
