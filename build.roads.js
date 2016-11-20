c0cf103f0bb430e180f0a93277544ab5403e5845
sondrev <sonves@gmail.com>
2016-11-20 01:32:51 +0100

    s

--- build.roads.js
+++ build.roads.js
@@ -34,16 +34,20 @@ var buildRoads = {
       var path = new PathFinder.search(pos1,{pos:pos2,range:1},{plainCost: 1,swampCost: 1,roomCallback: function(roomName) {return buildRoads.getCallback(roomName)}} );
         for (i = 0; i < path.path.length; i++) {
             let pos = path.path[i];
-            Game.rooms[pos1.roomName].createConstructionSite(pos.x,pos.y,STRUCTURE_ROAD);
+            if (Game.rooms[pos.roomName]!=undefined) {
+              Game.rooms[pos.roomName].createConstructionSite(pos.x,pos.y,STRUCTURE_ROAD);
+            } else console.log('undefined room ',pos.roomName);
         }
     },
     buildDoubleRoad: function(pos1,pos2) {
       var path = new PathFinder.search(pos1,{pos:pos2,range:1},{plainCost: 1,swampCost: 1,roomCallback: function(roomName) {return buildRoads.getCallbackDoubleroad(roomName)}} );
         for (i = 0; i < path.path.length; i++) {
             let pos = path.path[i];
-            Game.rooms[pos1.roomName].createConstructionSite(pos.x,pos.y,STRUCTURE_ROAD);
-            Game.rooms[pos1.roomName].createConstructionSite(pos.x-1,pos.y,STRUCTURE_ROAD);
-            Game.rooms[pos1.roomName].createConstructionSite(pos.x,pos.y-1,STRUCTURE_ROAD);
+            if (Game.rooms[pos.roomName]!=undefined) {
+              Game.rooms[pos.roomName].createConstructionSite(pos.x,pos.y,STRUCTURE_ROAD);
+              Game.rooms[pos.roomName].createConstructionSite(pos.x-1,pos.y,STRUCTURE_ROAD);
+              Game.rooms[pos.roomName].createConstructionSite(pos.x,pos.y-1,STRUCTURE_ROAD);
+            } else console.log('undefined room ',pos.roomName);
         }
     },
 
@@ -70,50 +74,29 @@ var buildRoads = {
         //BYGG vei til kilder i trygge rom
         var scout=room.memory.scout;
         for (var key1 in scout) {
-          if (scout.hasOwnProperty(key1)) {
             if (scout[key1].danger==0) {
               var sources = scout[key1].sources;
               if ((Game.rooms[key1]!=undefined) && !Game.rooms[key1].find(FIND_MY_SPAWNS)[0]) shouldBuildRoadsToSpawns=true; else shouldBuildRoadsToSpawns=false;
               if (shouldBuildRoadsToSpawns) {
                 for (var key2 in sources) {
-                  if (sources.hasOwnProperty(key2)) {
                     var source = sources[key2];
                     var posX = source.pos.x;
                     var posY = source.pos.y;
                     var posRoom = key1;
                     var targetPos = new RoomPosition(posX,posY,posRoom);
-                    var path = new PathFinder.search(posSpawn,{pos:targetPos,range:1},{plainCost: 1,swampCost: 1,roomCallback: function(roomName) {return buildRoads.getCallback(roomName)}} );
-                    if (path) {
-                      for (i = 0; i < path.path.length; i++) {
-                          let pos = path.path[i];
-                          if (Game.rooms[pos.roomName]!=undefined) {
-                            Game.rooms[pos.roomName].createConstructionSite(pos.x,pos.y,STRUCTURE_ROAD);
-                          } else {console.log('undefined');}
-                      }
-                    }
-                  }
+                    buildRoads.buildRoad(posSpawn,targetPos);
                 }
             } else if (Game.rooms[key1]){   //SHOULD BUILD ROAD TO OWN ROOM
               var target = Game.rooms[key1].find(FIND_MY_SPAWNS)[0];
               if (target) {
-                var path = new PathFinder.search(posSpawn,{pos:target.pos,range:1},{plainCost: 1,swampCost: 1,roomCallback: function(roomName) {return buildRoads.getCallback(roomName)}} );
-                if (path) {
-                for (i = 0; i < path.path.length; i++) {
-                    let pos = path.path[i];
-                    if (Game.rooms[pos.roomName]!=undefined) {
-                      Game.rooms[pos.roomName].createConstructionSite(pos.x,pos.y,STRUCTURE_ROAD);
-                    } else {console.log('undefined');}
-                }
+                buildRoads.buildRoad(posSpawn,target.pos);
               }
-              }
-
-
 
             }
 
             }
 
-          }
+
         }
 
 
