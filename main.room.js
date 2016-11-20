6e5f64cdea86ff129dbd9e406e5cf378a8090de4
sondrev <sonves@gmail.com>
2016-11-20 22:42:32 +0100

    s

--- main.room.js
+++ main.room.js
@@ -95,7 +95,7 @@ var mainRoom = {
         if (containers.length>=1&&extensions.length>=3) buildRoads.run(room);
       }
       if (room.memory.roomdesign==2) {
-        //if (containers.length>=1&&extensions.length>=3) buildRoads.run(room);
+        if (containers.length>=1&&extensions.length>=3) buildRoads.run(room);
       }
 
       room.memory.timeToRecheck=100;
