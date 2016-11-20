c978edfa5becf79fb75faca3b40ff2f573b08b47
sondrev <sonves@gmail.com>
2016-11-20 17:46:19 +0100

    s

--- tasks.js
+++ tasks.js
@@ -2,8 +2,8 @@ var tasks = {
     enterRoom: function(creep) {
       if (creep.pos.x==0) {creep.move(RIGHT);
       } else if (creep.pos.x==49) {creep.move(LEFT);
-    } else if (creep.pos.y==0) {creep.move(TOP);
-  } else if (creep.pos.y==49) {creep.move(BOTTOM);}
+      } else if (creep.pos.y==49) {creep.move(TOP);
+      } else if (creep.pos.y==0) {creep.move(BOTTOM);}
 
     },
     scoutRoom: function(creep) {
