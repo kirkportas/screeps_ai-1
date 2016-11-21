3487c74fb980baf7a3f5d6ea5cff01cae2778fd3
sondrev <sonves@gmail.com>
2016-11-21 11:07:01 +0100

    s

--- main.spawn.js
+++ main.spawn.js
@@ -90,6 +90,7 @@ var mainSpawn = {
           if ((Game.rooms[roomName]) && Game.rooms[roomName].find(FIND_MY_SPAWNS)[0]) continue; //Dont send to own room
           for (var sourceId in sources) {
             if (!sources[sourceId].pathLen) continue;
+            let remoteBuilders = _.filter(Game.creeps, (creep) => creep.memory.homeRoom == spawn.room.name && creep.memory.targetRoom == roomName && creep.memory.role == 'remoteBuilder' ).length;
             let harvestersRemote = _.filter(Game.creeps, (creep) => creep.memory.homeRoom == spawn.room.name && creep.memory.targetRoom == roomName && creep.memory.role == 'remoteHarvester' && creep.memory.pref == sourceId).length;
             var size = Math.floor((spawn.room.energyCapacityAvailable)/200);
             var carrycap= (size*50)
@@ -100,7 +101,7 @@ var mainSpawn = {
             var needed=Math.round(obtainable/sourcePerTick);
             if (sourceId=='57ef9ea486f108ae6e60fa55') needed=1;
             //console.log('data: '+spawn.room+' '+roomName+' '+sourceId+' '+' '+harvestersRemote+' '+needed);
-            if (harvestersRemote<needed) {
+            if (harvestersRemote+remoteBuilders<needed) {
               createCreepAdvanced(spawn,'remoteHarvester',createBody({move:size,carry:size,work:size}),{targetRoom:roomName, pref: sourceId, spawnerAction: "none"});
               return true;
             }
