f08c7644634488d294a83efc9aecc73f0df4be7c
sondrev <sonves@gmail.com>
2016-11-21 02:05:44 +0100

    s

--- build.roads.js
+++ build.roads.js
@@ -20,8 +20,8 @@ var buildRoads = {
       room.find(FIND_STRUCTURES).forEach(function(structure) {
         if (structure.structureType == STRUCTURE_WALL) {
           costs.set(structure.pos.x, structure.pos.y, 255);
-          costs.set(structure.pos.x+1, structure.pos.y, Math.max(128,costs.get(structure.pos.x+1, structure.pos.y)));
-          costs.set(structure.pos.x, structure.pos.y+1, Math.max(128,costs.get(structure.pos.x, structure.pos.y+1)));
+          costs.set(structure.pos.x+1, structure.pos.y, costs.get(structure.pos.x+1, structure.pos.y)+64);
+          costs.set(structure.pos.x, structure.pos.y+1, costs.get(structure.pos.x, structure.pos.y+1)+64);
         }
       });
       return costs;
