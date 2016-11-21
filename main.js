4e3a329dc4f83265ea491fd9aad211c25a12d057
sondrev <sonves@gmail.com>
2016-11-21 11:42:26 +0100

    s

--- main.js
+++ main.js
@@ -96,7 +96,7 @@ module.exports.loop = function () {
       }
   }
 
-    if (cpu.bucket<9500) console.log('Bucket: ',cpu.bucket);
+    if (cpu.bucket<19500) console.log('Bucket: ',cpu.bucket);
     console.log('CPU('+cpu.getUsed().toFixed(2)+'): room: '+timeRoom.toFixed(2)+', tower: '+timeTower.toFixed(2)+', spawn: '+timeSpawn.toFixed(2)+', AI: '+timeAI.toFixed(2)+' (har: '+timeHarvester.toFixed(2)+').');
 
 }
