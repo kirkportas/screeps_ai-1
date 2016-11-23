2c9d733bb3864a47a42c3a39f27738149b2eb801
sondrev <sonves@gmail.com>
2016-11-23 23:13:46 +0100

    s

--- role.builder.js
+++ role.builder.js
@@ -2,10 +2,21 @@ var tasks = require('tasks');
 var roleBuilder = {
 
   findBuild: function(creep) {
+    var targetsAll = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
     creep.say('r')
   },
   findRepair: function(creep) {
     creep.say('r')
+  },
+  repairTarget: function(creep) {
+    if(creep.repair(Game.getObjectById(creep.memory.targetFix)) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(Game.getObjectById(creep.memory.targetFix));
+    }
+  },
+  buildTarget: function(creep) {
+    if(creep.build(targetsAll) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(targetsAll);
+    }
   },
 
   run: function(creep) {
@@ -20,8 +31,8 @@ var roleBuilder = {
     }
 
     if (creep.memory.building) {
-      if (creep.memory.targetFix) { roleBuilder.repairTarget(creep))
-      } else if (creep.memory.targetBuild) { roleBuilder.buildTarget(creep))
+      if (creep.memory.targetFix) { roleBuilder.repairTarget(creep)
+      } else if (creep.memory.targetBuild) { roleBuilder.buildTarget(creep)
       } else if (findBuild()) {
 
       } else if (findRepair()) {
