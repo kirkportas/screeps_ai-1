7b4807950974166ab53bba944cef2ebf8435e3ee
sondrev <sonves@gmail.com>
2016-11-23 23:26:55 +0100

    s

--- role.builder.js
+++ role.builder.js
@@ -2,12 +2,12 @@ var tasks = require('tasks');
 var roleBuilder = {
 
   findBuild: function(creep) {
+    console.log('1')
     var targetsAll = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
     return false;
-    creep.say('r')
   },
   findRepair: function(creep) {
-    creep.say('r')
+    console.log('2')
 
     var target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
        filter: struct => (struct.hits<struct.hitsMax*0.5 && struct.structureType!=STRUCTURE_WALL && struct.structureType!=STRUCTURE_RAMPART)
@@ -20,6 +20,7 @@ var roleBuilder = {
       }
   },
   repairTarget: function(creep) {
+    console.log('3')
     var target = Game.getObjectById(creep.memory.targetFix);
     if(creep.repair(target) == ERR_NOT_IN_RANGE) {
         creep.moveTo(target);
@@ -27,6 +28,7 @@ var roleBuilder = {
     return true;
   },
   buildTarget: function(creep) {
+    console.log('4')
     var target = Game.getObjectById(creep.memory.targetBuild);
     if(creep.build(target) == ERR_NOT_IN_RANGE) {
         creep.moveTo(target);
