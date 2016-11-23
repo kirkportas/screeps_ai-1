9cbb7b5daf14648a71456ad280df81521190b5bd
sondrev <sonves@gmail.com>
2016-11-23 23:41:29 +0100

    s

--- role.builder.js
+++ role.builder.js
@@ -31,6 +31,7 @@ var roleBuilder = {
   },
   buildTarget: function(creep) {
     var target = Game.getObjectById(creep.memory.targetBuild);
+    if (target==null) {creep.memory.targetBuild=null;return false;}
     var r = creep.build(target);
     creep.say(target) //
     if(creep.build(target) == ERR_NOT_IN_RANGE) {
