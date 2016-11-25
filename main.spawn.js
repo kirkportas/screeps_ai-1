92234d0a3732e3990d086b824f8873c8718897f6
sondrev <sonves@gmail.com>
2016-11-25 01:52:09 +0100

    s

--- main.spawn.js
+++ main.spawn.js
@@ -114,15 +114,15 @@ var mainSpawn = {
   global.spawnArmy = function(spawn) {
     if (_.filter(Game.creeps, (creep)  => creep.memory.manual == '1').length<5) {
       createCreepAdvanced(spawn,'attacker',createBody({tough:7,move:7,heal:7}),{flag:'attack2',manual:'1'})
-      return false;
+      return true;
     }
     if (_.filter(Game.creeps, (creep)  => creep.memory.manual == '2').length<5) {
       createCreepAdvanced(spawn,'attacker',createBody({move:1}),{flag:'attack',manual:'2'})
-      return false;
+      return true;
     }
     if (_.filter(Game.creeps, (creep)  => creep.memory.manual == '3').length<5) {
       createCreepAdvanced(spawn,'attacker',createBody({tough:8,move:8,attack:8}),{flag:'attack2',manual:'3'})
-      return false;
+      return true;
     }
     return false;
   }
