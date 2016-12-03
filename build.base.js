eb68deb433a6d994d0ac9c32210e6e49ee6db45e
sondrev <sonves@gmail.com>
2016-12-03 20:47:51 +0100

    s

--- build.base.js
+++ build.base.js
@@ -2,7 +2,7 @@ var buildBase = {
     run: function(room) {
       if (room.controller.level>=6) {
         var minerals = room.find(FIND_MINERALS);
-        _.forEach(minerala, function(mineral){
+        _.forEach(minerals, function(mineral){
           room.createConstructionSite(mineral.pos,STRUCTURE_EXTRACTOR);
         });
       }
