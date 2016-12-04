d83b2a6d25d2b70668beac250132a78f829b5a72
sondrev <sonves@gmail.com>
2016-12-04 18:31:25 +0100

    s

--- role.remoteBuilder.js
+++ role.remoteBuilder.js
@@ -65,9 +65,16 @@ var roleRemotebuilder = {
               }
 
         } else {
-          var source = creep.pos.findClosestByPath(FIND_SOURCES,{filter: (source) => {return source.energy>0} });
-          if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
-              creep.moveTo(source);
+          var container = creep.pos.findClosestByPath(FIND_STRUCTURES,{filter:(structure)>=> {return structureType==STRUCTURE_CONTAINER && structure.energy>0} })[0]
+          if (container) {
+            if(creep.withdraw(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+              creep.moveTo(container);
+            }
+          } else {
+            var source = creep.pos.findClosestByPath(FIND_SOURCES,{filter: (source) => {return source.energy>0} });
+            if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(source);
+            }
           }
         }
 
