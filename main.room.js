0a00ae1354972c54c8954d45ee786c889322c0af
sondrev <sonves@gmail.com>
2016-12-05 17:24:55 +0100

    s

--- main.room.js
+++ main.room.js
@@ -49,7 +49,7 @@ var mainRoom = {
         //if (room.spawn.hits<room.spawn.hitsMax*0.6) triggerSafemode=true;
         _.forEach(room.find(FIND_STRUCTURES,{filter:(structure)=>{return structure.structureType==STRUCTURE_SPAWN||structure.structureType==STRUCTURE_TOWER||structure.structureType==STRUCTURE_STORAGE}}), function(structure){
           var closest=structure.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-          if (structure.inRangeTo(closest,3))
+          if (structure.pos.inRangeTo(closest,3))
           console.log('found something val')
         });
 
