a8ae28df074744182190bf80263e8051da2d8d0b
sondrev <sonves@gmail.com>
2016-12-05 17:27:01 +0100

    s

--- main.room.js
+++ main.room.js
@@ -47,9 +47,9 @@ var mainRoom = {
         console.log("Hostile in "+room.name);
         var triggerSafemode=false;
         //if (room.spawn.hits<room.spawn.hitsMax*0.6) triggerSafemode=true;
-        _.forEach(room.find(FIND_STRUCTURES,{filter:(structure)=>{return structure.structureType==STRUCTURE_SPAWN||structure.structureType==STRUCTURE_TOWER||structure.structureType==STRUCTURE_STORAGE}}), function(structure){
-          var closest=structure.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-          if (structure.pos.inRangeTo(closest,3))
+        _.forEach(room.find(FIND_STRUCTURES,{filter:(structure)=>{return structure.structureType==STRUCTURE_SPAWN||structure.structureType==STRUCTURE_TOWER||structure.structureType==STRUCTURE_STORAGE}}), function(structure2){
+          var closest=structure2.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+          if (structure2.pos.inRangeTo(closest,3))
           console.log('found something val')
         });
 
