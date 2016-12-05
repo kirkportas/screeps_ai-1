a3eb3b7188c879ee663ab42b8440bb52ac03f200
sondrev <sonves@gmail.com>
2016-12-05 10:40:59 +0100

    s

--- role.remoteHarvester.js
+++ role.remoteHarvester.js
@@ -28,7 +28,7 @@ var roleRemoteHarvester = {
         var container = creep.pos.findInRange(FIND_STRUCTURES,2,{ filter: (structure) => {return (structure.structureType == STRUCTURE_CONTAINER )  } })[0];
         var centralStorage=Game.rooms[creep.memory.homeRoom].find(FIND_STRUCTURES,8, {filter: (structure) => { return (structure.structureType == STRUCTURE_STORAGE)}})[0];
         if (container) {
-          if (container.hits<containers.hitsMax) creep.repair(container);
+          if (container.hits<container.hitsMax) creep.repair(container);
           tasks.deliverSourceDedicated(creep,containers);//
         } else if (centralStorage) {
           if(creep.transfer(centralStorage, RESOURCE_ENERGY)== OK) {
