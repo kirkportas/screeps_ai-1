49dd4469b6f3b105fb79df9b74a5aba5bbf2804d
sondrev <sonves@gmail.com>
2016-12-05 01:27:59 +0100

    s

--- role.extracter.js
+++ role.extracter.js
@@ -9,19 +9,22 @@ var roleExtracter = {
         if(creep.memory.delivering && creep.carry.energy == 0) {
               creep.memory.delivering = false;
         }
-        if(!creep.memory.delivering && creep.carry.energy ==creep.carryCapacity) {
+        if(!creep.memory.delivering && _.sum(creep.carry) == creep.carryCapacity)
             creep.memory.delivering = true;
         }
 
 	    if(!creep.memory.delivering) {
-
         var mineral=Game.getObjectById(creep.memory.mineral);
-        creep.say(creep.harvest(mineral))
         if (creep.harvest(mineral) == ERR_NOT_IN_RANGE) {
           creep.moveTo(mineral)
         }
       } else {
-
+        var terminal = creep.room.terminal;
+        for(var resourceType in creep.carry) {
+          if(creep.transfer(terminal, resourceType)== ERR_NOT_IN_RANGE) {
+              creep.moveTo(terminal);
+          }
+        }
 
       }
 	}
