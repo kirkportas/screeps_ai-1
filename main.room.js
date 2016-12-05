c53582e20fbd81eece2175a9545fe3e35fe16dcc
sondrev <sonves@gmail.com>
2016-12-05 02:36:01 +0100

    s

--- main.room.js
+++ main.room.js
@@ -21,10 +21,10 @@ var mainRoom = {
 
       global.viewOrders2 = function(roomName) {
         var targetRoom = "E65S61";
-        var orders = Game.market.getAllOrders(order => order.resourceType == RESOURCE_ENERGY &&order.type == ORDER_SELL &&  Game.market.calcTransactionCost(1000, targetRoom, order.roomName) < 5000);
+        var orders = Game.market.getAllOrders(order => order.resourceType == RESOURCE_ENERGY &&order.type == ORDER_SELL &&  Game.market.calcTransactionCost(1000, targetRoom, order.roomName) < 2000);
         Memory.test=orders;
         orders= _.sortBy(orders, o => o.price);
-        console.log(orders[0].price+" - "+Game.market.calcTransactionCost(100, targetRoom, orders[0].roomName))
+        console.log(orders[0].price+" - "+Game.market.calcTransactionCost(1000, targetRoom, orders[0].roomName))
         //var res= Game.market.deal(orders[0].id, 100, targetRoom);
         //console.log(res)
       }
