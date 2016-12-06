cc1ac0d14fba480fac63a6c3547c9c6d4998015a
sondrev <sonves@gmail.com>
2016-12-06 21:27:45 +0100

    s

--- tasks.js
+++ tasks.js
@@ -10,7 +10,7 @@ var tasks = {
       if (creep.room.name == creep.memory.targetRoom && Memory.rooms[creep.memory.targetRoom]) {
         if (!Memory.rooms[creep.memory.targetRoom].scoutFromOther) Memory.rooms[creep.memory.targetRoom].scoutFromOther={};
         //var scout=Memory.rooms[creep.memory.targetRoom].scoutFromOther;
-        //
+        
         var sources= creep.room.find(FIND_SOURCES);
         if (!Memory.rooms[creep.memory.targetRoom].scoutFromOther.sources) {
           Memory.rooms[creep.memory.targetRoom].scoutFromOther.sources={}
