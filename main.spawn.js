8ce1fd6eb0cd1ce46abd260a37dc7be1774570c8
sondrev <sonves@gmail.com>
2016-12-07 01:04:52 +0100

    s

--- main.spawn.js
+++ main.spawn.js
@@ -99,7 +99,7 @@ var mainSpawn = {
           if (!Memory.rooms[roomName].scoutFromOther) continue;
           var scoutFrom=Memory.rooms[roomName].scoutFromOther;
           console.log(scoutFrom)
-          if (!scoutFrom||(Game.rooms[roomName]) && Game.rooms[roomName].find(FIND_MY_SPAWNS)[0]) continue; //Dont send to own room
+          if (!scoutFrom||((Game.rooms[roomName]) && Game.rooms[roomName].find(FIND_MY_SPAWNS)[0])) continue; //Dont send to own room
           console.log('test')
           var sources = scoutFrom.sources;
           for (var sourceId in sources) {
