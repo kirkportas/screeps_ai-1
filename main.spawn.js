f4377b2fcf545f465fa6128ec5bccc865d526086
sondrev <sonves@gmail.com>
2016-12-07 22:31:45 +0100

    s

--- main.spawn.js
+++ main.spawn.js
@@ -97,6 +97,7 @@ var mainSpawn = {
       for (var roomName in scoutTo) {
           if (!Memory.rooms[roomName]) continue;
           if (!Memory.rooms[roomName].scoutFromOther) continue;
+          if (Memory.rooms[roomName].scoutFromOther.closestRoom!=room) continue;
           var scoutFrom=Memory.rooms[roomName].scoutFromOther;
           if (!scoutFrom||((Game.rooms[roomName]) && Game.rooms[roomName].find(FIND_MY_SPAWNS)[0])) continue; //Dont send to own room
           var sources = scoutFrom.from[spawn.room.name].sources;
