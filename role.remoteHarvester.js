77dfd0be6c4d24206dbe73ae48e9486dfac439e1
sondrev <sonves@gmail.com>
2016-12-07 01:42:36 +0100

    s

--- role.remoteHarvester.js
+++ role.remoteHarvester.js
@@ -14,18 +14,13 @@ var roleRemoteHarvester = {
 
 
       if(!creep.memory.delivering) {
-        if (!tasks.pickupEnergy(creep)) {
-          //if(creep.room.name != creep.memory.targetRoom) {
-
-              var source = Game.getObjectById(creep.memory.pref)
-              var result = creep.harvest(source)
-              if( source==null || result == ERR_NOT_IN_RANGE || result == ERR_NOT_ENOUGH_RESOURCES ) {
-                console.log(creep.name)
-                console.log(creep.memory.prefPos)
-                  creep.moveTo(new RoomPosition(creep.memory.prefPos.x,creep.memory.prefPos.y,creep.memory.prefPos.roomName));
-              }
-
-        } else if (creep.carry.energy == creep.carryCapacity*0.6) {creep.memory.delivering = true;}  //Picked up alot - should return?
+        var source = Game.getObjectById(creep.memory.pref)
+        var result = creep.harvest(source)
+        if( source==null || result == ERR_NOT_IN_RANGE || result == ERR_NOT_ENOUGH_RESOURCES ) {
+          console.log(creep.name)
+          console.log(creep.memory.prefPos)
+            creep.moveTo(new RoomPosition(creep.memory.prefPos.x,creep.memory.prefPos.y,creep.memory.prefPos.roomName));
+        }
       } else {
         var container = creep.pos.findInRange(FIND_STRUCTURES,2,{ filter: (structure) => {return (structure.structureType == STRUCTURE_CONTAINER )  } })[0];
         var centralStorage=Game.rooms[creep.memory.homeRoom].find(FIND_STRUCTURES,8, {filter: (structure) => { return (structure.structureType == STRUCTURE_STORAGE)}})[0];
