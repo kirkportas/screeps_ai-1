e0c5f631d94eaa073bf8179596d9d82f14148741
sondrev <sonves@gmail.com>
2016-12-07 23:24:03 +0100

    s

--- main.spawn.js
+++ main.spawn.js
@@ -95,8 +95,8 @@ var mainSpawn = {
     global.spawnRemoteHarvesters = function(spawn) {
       var scoutTo=spawn.room.memory.scout;
       for (var roomName in scoutTo) {
-          if (!Memory.rooms[roomName]) continue;
-          if (!Memory.rooms[roomName].scoutFromOther) continue;
+          //if (!Memory.rooms[roomName]) continue;
+          //if (!Memory.rooms[roomName].scoutFromOther) continue;
           //if (Memory.rooms[roomName].scoutFromOther.closestRoom!=spawn.room.name) {console.log('not best room '+spawn.room.name+' to '+roomName);continue; }
           var scoutFrom=Memory.rooms[roomName].scoutFromOther;
           if (!scoutFrom||((Game.rooms[roomName]) && Game.rooms[roomName].find(FIND_MY_SPAWNS)[0])) continue; //Dont send to own room
