2d4155c512a2d743f36137f239fb98694eccca2d
sondrev <sonves@gmail.com>
2016-12-08 18:16:23 +0100

    s

--- main.scout.js
+++ main.scout.js
@@ -2,7 +2,7 @@ var tasks = require('tasks');
 var mainScout = {
 
     run: function(room) {
-      if (room.memory.scout===undefined||room.name=='sE68S62') {
+      if (room.memory.scout===undefined||room.name=='E68S62') {
         console.log('setting basic scout info');
         var exits =Game.map.describeExits(room.name);
         var rooms=[]; //
@@ -23,7 +23,7 @@ var mainScout = {
 
         }
         console.log('-----')
-        for (var i=rooms.length;i<rooms2.length+rooms.length;i++) {
+        for (var i=0;i<rooms2.length;i++) {
           console.log(rooms2[i]);
           //room.memory.scout[rooms[rooms.length+i]]={dist:2,timeSinceLastScout:-1,timeSinceLastScout:-1, danger:-1,sources:{}};
         }
