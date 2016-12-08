f8e593c607023f82a14bca7c4c0427209eaa6c90
sondrev <sonves@gmail.com>
2016-12-08 02:00:28 +0100

    s

--- main.spawn.js
+++ main.spawn.js
@@ -119,7 +119,7 @@ var mainSpawn = {
             var reservation = scoutFrom.reservation;
             if (reservation<2000 && reservation>=0) {
               console.log(Object.keys(sources).length)
-              if (sources.length>=2) {  // do I WANT to claim this room?
+              if (Object.keys(sources).length>=2) {  // do I WANT to claim this room?
 
                 let claimers = _.filter(Game.creeps, (creep) => creep.memory.homeRoom == spawn.room.name && creep.memory.targetRoom == roomName && creep.memory.role == 'claimer').length;
                 let size= Math.min(3,Math.floor(energyAvav/650));
