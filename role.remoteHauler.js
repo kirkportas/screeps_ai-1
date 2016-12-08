5f6c17d09e34ddbc444923acf633cc802e0bd91d
sondrev <sonves@gmail.com>
2016-12-09 00:35:28 +0100

    s

--- role.remoteHauler.js
+++ role.remoteHauler.js
@@ -6,7 +6,7 @@ Creep.prototype.moveToOpt = function(target) {
   var oldPos=this.memory.oldPos;
   if (oldPos!=undefined&&curPos.x==oldPos.x&&curPos.y==oldPos.y) {
     if (this.memory.stuckTime) this.memory.stuckTime=this.memory.stuckTime+1; else this.memory.stuckTime=1;
-    if (this.memory.stuckTime>=2) {
+    if (this.memory.stuckTime>=3) {
       this.say('stuck')
       console.log('stuck in ',this.room.name)
       this.moveTo(target,{ignoreCreeps:false,reusePath:5});
