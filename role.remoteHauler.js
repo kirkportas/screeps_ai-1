3d802ddfbbfcf46d4c956383dfa5b495690feb1c
sondrev <sonves@gmail.com>
2016-12-09 00:52:16 +0100

    s

--- role.remoteHauler.js
+++ role.remoteHauler.js
@@ -1,24 +1,6 @@
 var tasks = require('tasks');
 var prototypeCreep = require('prototype.creep');
 
-Creep.prototype.moveToOpt = function(target) {
-
-  var curPos=this.pos;
-  var oldPos=this.memory.oldPos;
-  if (oldPos!=undefined&&curPos.x==oldPos.x&&curPos.y==oldPos.y) {
-    if (this.memory.stuckTime) this.memory.stuckTime=this.memory.stuckTime+1; else this.memory.stuckTime=1;
-    if (this.memory.stuckTime>=3) {
-      this.say('stuck')
-      console.log('stuck in ',this.room.name)
-      this.moveTo(target,{ignoreCreeps:false,reusePath:5});
-    }
-
-  } else {
-      this.memory.stuckTime=0;
-      this.moveTo(target,{ignoreCreeps:true,reusePath:100});
-  }
-  this.memory.oldPos=curPos;
-}
 
 Creep.prototype.runRemoteHauler = function(creep) {
   var hostiles = creep.room.find(FIND_HOSTILE_CREEPS,{filter: (hostile) => { return (hostile.getActiveBodyparts(ATTACK)+hostile.getActiveBodyparts(RANGED_ATTACK)>0)}});
