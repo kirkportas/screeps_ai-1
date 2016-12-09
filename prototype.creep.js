0738c61bcae4d6da63a96eb7e64a9ece950f45ff
sondrev <sonves@gmail.com>
2016-12-09 01:03:52 +0100

    s

--- prototype.creep.js
+++ prototype.creep.js
@@ -2,7 +2,7 @@ Creep.prototype.moveToOpt = function(target) {
 
   var curPos=this.pos;
   var oldPos=this.memory.oldPos;
-  if (oldPos!=undefined&&curPos.x==oldPos.x&&curPos.y==oldPos.y) {
+  if (oldPos!=undefined&&curPos.x==oldPos.x&&curPos.y==oldPos.y&&this.fatigue>0) {
     if (this.memory.stuckTime) this.memory.stuckTime=this.memory.stuckTime+1; else this.memory.stuckTime=1;
     if (this.memory.stuckTime>=3) {
       this.say('stuck')
