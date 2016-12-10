e5b3cc56b8614dbbe9b81032bb319e4518fc6e24
sondrev <sonves@gmail.com>
2016-12-11 00:15:52 +0100

    s

--- prototype.creep.js
+++ prototype.creep.js
@@ -1,25 +1,24 @@
-Creep.prototype.moveToOpt = function(target) {
-  var getCallback= function(roomName) {
-      console.log('callback')
-        let room = Game.rooms[roomName];
-        if (!room||room.isSourceKeeperRoom()) return;
-        let costs = new PathFinder.CostMatrix;
+Creep.prototype.getCallback = function(target) {
+  console.log('callback')
+    let room = Game.rooms[roomName];
+    if (!room||room.isSourceKeeperRoom()) return;
+    let costs = new PathFinder.CostMatrix;
 
-        room.find(FIND_STRUCTURES).forEach(function(structure) {
-          if (structure.structureType === STRUCTURE_ROAD) {
-            // Favor roads over plain tiles
-            costs.set(structure.pos.x, structure.pos.y, 1);
-          } else if (structure.structureType !== STRUCTURE_CONTAINER &&
-                     (structure.structureType !== STRUCTURE_RAMPART ||
-                      !structure.my)) {
-            // Can't walk through non-walkable buildings
-            costs.set(structure.pos.x, structure.pos.y, 0xff);
-          }
-        });
-        return costs;
+    room.find(FIND_STRUCTURES).forEach(function(structure) {
+      if (structure.structureType === STRUCTURE_ROAD) {
+        // Favor roads over plain tiles
+        costs.set(structure.pos.x, structure.pos.y, 1);
+      } else if (structure.structureType !== STRUCTURE_CONTAINER &&
+                 (structure.structureType !== STRUCTURE_RAMPART ||
+                  !structure.my)) {
+        // Can't walk through non-walkable buildings
+        costs.set(structure.pos.x, structure.pos.y, 0xff);
       }
+    });
+    return costs;
+  }
 
-
+Creep.prototype.moveToOpt = function(target) {
   if (this.fatigue>0) return;
   var curPos=this.pos;
   var oldPos=this.memory.oldPos;
@@ -28,12 +27,12 @@ Creep.prototype.moveToOpt = function(target) {
     if (this.memory.stuckTime>=2) {
       this.say('stuck')
       console.log('stuck in ',this.room.name)
-      this.moveTo(target,{ignoreCreeps:false,reusePath:5,plainCost: 2,swampCost: 4,roomCallback: function(roomName) {return getCallback2(roomName)}});
+      this.moveTo(target,{ignoreCreeps:false,reusePath:5,plainCost: 2,swampCost: 4,roomCallback: function(roomName) {return this.getCallback(roomName)}});
     }
 
   } else {
       this.memory.stuckTime=0;
-      this.moveTo(target,{ignoreCreeps:true,reusePath:200,plainCost: 2,swampCost: 4,roomCallback: function(roomName) {return getCallback2(roomName)}});
+      this.moveTo(target,{ignoreCreeps:true,reusePath:200,plainCost: 2,swampCost: 4,roomCallback: function(roomName) {return this.getCallback(roomName)}});
   }
   this.memory.oldPos=curPos;
 }
