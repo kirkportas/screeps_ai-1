ab6272dfd78223df4a42463369cb1ceccfd72273
sondrev <sonves@gmail.com>
2016-12-10 02:48:32 +0100

    s

--- role.spawnhauler.js
+++ role.spawnhauler.js
@@ -67,7 +67,7 @@ Creep.prototype.runSpawnhauler = function(creep) {
         var centralStorage=creep.room.storage;
         if (centralStorage) {
           if (!linkUpgrade) {
-            var centralLink=centralStorage.pos.findInRange(FIND_STRUCTURES,5, {filter: (structure) => {return (structure.structureType == STRUCTURE_LINK && structure.energy>=400) }});
+            var centralLink=centralStorage.pos.findInRange(FIND_STRUCTURES,5, {filter: (structure) => {return (structure.structureType == STRUCTURE_LINK && structure.energy>=200) }});
             target = target.concat(centralLink);
           }
           target = target.concat(centralStorage);
