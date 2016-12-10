80c4a573f68f44ae4255420c46b7263725344cb8
sondrev <sonves@gmail.com>
2016-12-11 00:04:04 +0100

    s

--- prototype.room.js
+++ prototype.room.js
@@ -1,5 +1,6 @@
 Room.prototype.isSourceKeeperRoom = function() {
-  console.log('test')
+  console.log('tests')
   if (this.name=='E65S63') return true;
+  if (this.name=='E64S64') return true;
   return false;
 }
