e353b7762919163e67f9cdb959c3158c5b1ee12c
sondrev <sonves@gmail.com>
2016-12-10 23:52:07 +0100

    s

--- prototype.creep.js
+++ prototype.creep.js
@@ -1,4 +1,21 @@
 Creep.prototype.moveToOpt = function(target) {
+  var getCallback= function(roomName) {
+        let room = Game.rooms[roomName];
+        if (!room) return;
+        let costs = new PathFinder.CostMatrix;
+
+        room.find(FIND_STRUCTURES).forEach(function(structure) {
+          if (structure.structureType == STRUCTURE_WALL) {
+            costs.set(structure.pos.x, structure.pos.y, 255);
+          }
+          if (structure.structureType == STRUCTURE_RAMPART) {
+            costs.set(structure.pos.x, structure.pos.y, 1);
+          }
+        });
+        return costs;
+      }
+
+
   if (this.fatigue>0) return;
   var curPos=this.pos;
   var oldPos=this.memory.oldPos;
@@ -7,12 +24,12 @@ Creep.prototype.moveToOpt = function(target) {
     if (this.memory.stuckTime>=2) {
       this.say('stuck')
       console.log('stuck in ',this.room.name)
-      this.moveTo(target,{ignoreCreeps:false,reusePath:5});
+      this.moveTo(target,{ignoreCreeps:false,reusePath:5,plainCost: 2,swampCost: 4,roomCallback: function(roomName) {return buildRoads.getCallback(roomName)}});
     }
 
   } else {
       this.memory.stuckTime=0;
-      this.moveTo(target,{ignoreCreeps:true,reusePath:100});
+      this.moveTo(target,{ignoreCreeps:true,reusePath:200,plainCost: 2,swampCost: 4,roomCallback: function(roomName) {return buildRoads.getCallback(roomName)}});
   }
   this.memory.oldPos=curPos;
 }
