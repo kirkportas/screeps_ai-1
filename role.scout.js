7547fcf10156f7b173a68b502ce4e518b4b0187b
sondrev <sonves@gmail.com>
2016-12-10 21:07:36 +0100

    s

--- role.scout.js
+++ role.scout.js
@@ -2,9 +2,13 @@ var tasks = require('tasks');
 var prototypeCreep = require('prototype.creep');
 Creep.prototype.runScout = function(creep) {
     if(creep.room.name != creep.memory.targetRoom) {
-      var exitDir = Game.map.findExit(creep.room, creep.memory.targetRoom);
-      var exit = creep.pos.findClosestByRange(exitDir);
-      creep.moveToOpt(exit);
+      if (creep.memory.targetExit) {
+        creep.moveToOpt(Game.getObjectById(creep.memory.targetExit));
+      } else {
+        var exitDir = Game.map.findExit(creep.room, creep.memory.targetRoom);
+        creep.memory.targetExit = creep.pos.findClosestByRange(exitDir);
+        creep.moveToOpt(Game.getObjectById(creep.memory.targetExit));
+      }
     } else {
 
       if (creep.moveTo(Game.rooms[creep.memory.targetRoom].controller) == ERR_NO_PATH) {
