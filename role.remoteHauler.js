243add079ba311675d05ab182ad8bee0e51515b0
sondrev <sonves@gmail.com>
2016-12-11 00:52:01 +0100

    s

--- role.remoteHauler.js
+++ role.remoteHauler.js
@@ -3,10 +3,14 @@ var prototypeCreep = require('prototype.creep');
 
 Creep.prototype.runRemoteHauler = function(creep) {
 
-  var repairRoads = function(creep) {
-    //var found = creep.room.lookForAtArea(LOOK_STRUCTURES,Math.max(0,creep.pos.y-2),Math.max(0,creep.pos.x-2),Math.min(49,creep.pos.y+2),Math.min(49,creep.pos.x+2));
-    var foundStruc = creep.pos.findInRange(FIND_STRUCTURES,3,{filter:(structure)=>{return (structure.structureType==STRUCTURE_ROAD&&structure.hits<structure.hitsMax)}})
-    var foundConst= creep.pos.findInRange(FIND_CONSTRUCTION_SITES,3,{filter:(structure)=>{return (structure.structureType==STRUCTURE_ROAD)}})
+  var repairRoads = function(creep) { //
+    var foundStruc = creep.room.lookForAtArea(LOOK_STRUCTURES,Math.max(0,creep.pos.y-2),Math.max(0,creep.pos.x-2),Math.min(49,creep.pos.y+2),Math.min(49,creep.pos.x+2));
+    var foundConst = creep.room.lookForAtArea(LOOK_CONSTRUCTION_SITES,Math.max(0,creep.pos.y-2),Math.max(0,creep.pos.x-2),Math.min(49,creep.pos.y+2),Math.min(49,creep.pos.x+2));
+    foundStruc = _.filter(foundStruc, (strc)    => strc.structureType==STRUCTURE_ROAD);
+    foundConst = _.filter(foundConst, (strc)    => strc.structureType==STRUCTURE_ROAD);
+
+    //var foundStruc = creep.pos.findInRange(FIND_STRUCTURES,3,{filter:(structure)=>{return (structure.structureType==STRUCTURE_ROAD&&structure.hits<structure.hitsMax)}})
+    //var foundConst= creep.pos.findInRange(FIND_CONSTRUCTION_SITES,3,{filter:(structure)=>{return (structure.structureType==STRUCTURE_ROAD)}})
     //  {filter:(structure)=>{return (strcture.structureType==STRUCTURE_ROAD&&structure.hits<structure.hitsMax)}}
     if (foundStruc.length) {
       creep.repair(foundStruc[0]);
