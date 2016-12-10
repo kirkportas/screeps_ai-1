a05f179a2de926553da7173142f37c01028b15c6
sondrev <sonves@gmail.com>
2016-12-10 20:49:09 +0100

    s

--- main.spawn.js
+++ main.spawn.js
@@ -141,8 +141,8 @@ StructureSpawn.prototype.spawnRemoteHarvesters = function() {
     }
     //ATTACK (defend)
     if (scoutFrom.danger==1&&Memory.rooms[roomName].scoutFromOther.closestRoom==this.room.name) {
-      if (!scoutFrom.lastAttackerSent || ((Game.time-scoutFrom.lastAttackerSent)>1)) {
-        var size = Math.min(15,Math.floor(((energyAvav-400/180))*0.80));
+      if (!scoutFrom.lastAttackerSent || ((Game.time-scoutFrom.lastAttackerSent)>5)) {
+        var size = Math.min(15,Math.floor((((energyAvav-400)/180))*0.80));
         console.log(size)
         if (this.createCreepAdvanced(this,'attacker',this.createBody({move:size+2,attack:size,rangedAttack:2}),{targetRoom:roomName,fleeAfter:true})) {
           console.log('sending a attacker to ',roomName);
