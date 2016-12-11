4212309a5733eeef6c230f5e87fc63a81e54a6f3
sondrev <sonves@gmail.com>
2016-12-11 19:52:06 +0100

    s

--- role.remoteHauler.js
+++ role.remoteHauler.js
@@ -18,8 +18,8 @@ Creep.prototype.runRemoteHauler = function(creep) {
     //var foundConst= creep.pos.findInRange(FIND_CONSTRUCTION_SITES,3,{filter:(structure)=>{return (structure.structureType==STRUCTURE_ROAD)}})
     //  {filter:(structure)=>{return (strcture.structureType==STRUCTURE_ROAD&&structure.hits<structure.hitsMax)}}
     if (foundStruc) {
-      Memory.test=_.find(creep.room.lookForAtArea(LOOK_STRUCTURES,upper,left,lower,right,true))
-      var test=_.find(creep.room.lookForAtArea(LOOK_STRUCTURES,upper,left,lower,right,true));
+      Memory.test=creep.room.lookForAtArea(LOOK_STRUCTURES,upper,left,lower,right,true)
+      var test=creep.room.lookForAtArea(LOOK_STRUCTURES,upper,left,lower,right,true);
       console.log(test.length)
       creep.repair(foundStruc);
     } else if (foundConst) {
