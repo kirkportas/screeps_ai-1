1026a31ea217b11c3e81b4863526b7e59c2118f6
sondrev <sonves@gmail.com>
2016-12-11 14:32:48 +0100

    s

--- role.remoteBuilder.js
+++ role.remoteBuilder.js
@@ -65,7 +65,6 @@ Creep.prototype.runRemoteBuilder = function(creep) {
         var foundDismantle=false;
         var flag = Game.flags['dis']
         if (flag&&flag.pos.roomName==creep.room.name) {
-          creep.say('found dis')
           var target=flag.pos.findInRange(FIND_STRUCTURES,1);
           if (target.length) {
             foundDismantle=true;
